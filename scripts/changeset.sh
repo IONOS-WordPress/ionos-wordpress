167a46002349aad8ede3082f0b0e43813d7155bb
Lars Gersmann <lars.gersmann@ionos.com>
2024-09-26 16:46:36 +0200

    refactor: integrated stub for post processing after changest versioning

--- scripts/changeset.sh
+++ scripts/changeset.sh
@@ -13,10 +13,23 @@ if [[ "$@" == '' ]]; then
   # if no arguments are passed, show help
   # prefix 'changeset' with 'pnpm ' in output to avoid user confusion
   pnpm exec changeset --help | sed 's/\$ \(changeset \)/pnpm \1/g'
-else
-  # otherwise delegate all arguments to wp-env
-  pnpm exec changeset ${@:---help}
+  exit 0
 fi
+
+# otherwise delegate all arguments to wp-env
+pnpm exec changeset $@
+
+# if versioning occured
+if [[ "$1" == 'version' ]]; then
+  # loop over all plugin.php files in ./packages/wp-plugin/*/
+  for plugin_php in $(find ./packages/wp-plugin -maxdepth 2 -type f -name "package.json"); do
+    # @TODO: update data from package.json in plugin.php, readme.txt using a function from bootstrap.sh
+    echo "$plugin_php"
+  done
+fi
+
+
+
 
 
 
