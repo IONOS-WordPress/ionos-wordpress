34549625568debab935b6fc1d3cb787629809268
Lars Gersmann <lars.gersmann@gmail.com>
2024-11-09 21:06:43 +0100

    refactor: ensure chromium browser is installed in a hermetic environment for headless playwright tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,8 +9,11 @@
 # bootstrap the environment
 source "$(realpath $0 | xargs dirname)/includes/bootstrap.sh"
 
-# ensure chromium browser is avaible for headless playwright test
-pnpx playwright install chromium
+#region ensure chromium browser is avaible for headless playwright test
+# PLAYWRIGHT_BROWSERS_PATH=0 places binaries to node_modules/playwright-core/.local-browsers
+# see https://playwright.dev/docs/browsers#hermetic-install
+PLAYWRIGHT_BROWSERS_PATH=0 pnpx playwright install chromium
+
 
 # start storybook
 pnpm exec playwright test -c ./playwright-ct.config.js $@
