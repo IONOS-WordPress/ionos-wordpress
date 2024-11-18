bb9f49c445974bc271674f854d62cc40c3e4542c
Lars Gersmann <lars.gersmann@ionos.com>
2024-11-18 12:44:02 +0100

    chore: add checks to prevent wp-env from running during cleanup

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -9,6 +9,17 @@
 # bootstrap the environment
 source "$(realpath $0 | xargs dirname)/includes/bootstrap.sh"
 
+#region ensure wp-env is not running
+# ensure wp-env is not running
+# - if the install path does not exist
+# - and the wp-env containers are not running
+WPENV_INSTALLPATH="$(realpath --relative-to $(pwd) $(pnpm exec wp-env install-path))"
+if [[ -d "$WPENV_INSTALLPATH/WordPress" ]] && [[ "$(docker ps -q --filter "name=$(basename $WPENV_INSTALLPATH)" | wc -l)" == '6' ]]; then
+  ionos.wordpress.log_warn "wp-env is already running. Excecute 'pnpm stop' or 'pnpm destroy' to stop it before cleaning up."
+  exit 1
+fi
+#endregion
+
 git clean $GIT_CLEAN_OPTS \
   -e '!/wp-env-home' \
   -e '!/*.code-workspace' \
