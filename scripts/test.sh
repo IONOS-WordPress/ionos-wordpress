028aa81a4c8f880e1272146646909fa9f61098fc
Lars Gersmann <lars.gersmann@ionos.com>
2024-11-18 12:06:10 +0100

    chore: ensure playwright cache consistency across environments

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,8 +9,20 @@
 # bootstrap the environment
 source "$(realpath $0 | xargs dirname)/includes/bootstrap.sh"
 
+# ensure the playwright cache is generated in the same environment (devcontainer or local) as the tests are executed
+# (this is necessary because the cache is not portable between environments)
+(
+  PLAYWRIGHT_DIR=$(realpath ./playwright)
+  if [[ -f "$PLAYWRIGHT_DIR/.cache/metainfo.json" ]] && ! grep "$PLAYWRIGHT_DIR" ./playwright/.cache/metainfo.json > /dev/null; then
+    # ./playwright/.cache/metainfo.json contains not the absolute path to the cache directory of the current environment
+    rm -rf "$PLAYWRIGHT_DIR/.cache"
+  fi
+)
+
 # execute playwright tests
 pnpm exec playwright test -c ./playwright-ct.config.js $@
+
+exit 0
 
 WPENV_INSTALLPATH="$(realpath --relative-to $(pwd) $(pnpm exec wp-env install-path))"
 
