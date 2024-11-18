b60e80dcb52544b89c1dba0dd51607e9d92d6151
Lars Gersmann <lars.gersmann@ionos.com>
2024-11-18 12:15:15 +0100

    chore: refactor playwright cache cleanup logic for clarity

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,15 +9,14 @@
 # bootstrap the environment
 source "$(realpath $0 | xargs dirname)/includes/bootstrap.sh"
 
-# ensure the playwright cache is generated in the same environment (devcontainer or local) as the tests are executed
+#region:  ensure the playwright cache is generated in the same environment (devcontainer or local) as the tests are executed
 # (this is necessary because the cache is not portable between environments)
-(
-  PLAYWRIGHT_DIR=$(realpath ./playwright)
-  if [[ -f "$PLAYWRIGHT_DIR/.cache/metainfo.json" ]] && ! grep "$PLAYWRIGHT_DIR" ./playwright/.cache/metainfo.json > /dev/null; then
-    # ./playwright/.cache/metainfo.json contains not the absolute path to the cache directory of the current environment
-    rm -rf "$PLAYWRIGHT_DIR/.cache"
-  fi
-)
+PLAYWRIGHT_DIR=$(realpath ./playwright)
+if [[ -f "$PLAYWRIGHT_DIR/.cache/metainfo.json" ]] && ! grep "$PLAYWRIGHT_DIR" ./playwright/.cache/metainfo.json > /dev/null; then
+  # ./playwright/.cache/metainfo.json contains not the absolute path to the cache directory of the current environment
+  rm -rf "$PLAYWRIGHT_DIR/.cache"
+fi
+#endregion
 
 # execute playwright tests
 pnpm exec playwright test -c ./playwright-ct.config.js $@
