e2c6992585cdda40c12a21acc4bf43e89cf2ed61
Lars Gersmann <lars.gersmann@ionos.com>
2024-11-20 15:40:04 +0100

    fix: added directories to skip to ecs config

--- rector-config-php7.4.php
+++ rector-config-php7.4.php
@@ -3,14 +3,10 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\DowngradeLevelSetList;
 // use Rector\ValueObject\PhpVersion;
 
 return RectorConfig::configure()
-  ->withSkip([
-    __DIR__ . '/dist/vendor',
-    __DIR__ . '/dist/languages',
-  ])
+  ->withSkip([__DIR__ . '/dist/vendor', __DIR__ . '/dist/languages'])
   ->withParallel()
   // see https://github.com/rectorphp/rector-src/blob/3ed476b9ab65958d85416e48a810b11dbaf4283a/build/config/config-downgrade.php
   //->withPHPStanConfigs([__DIR__ . '/phpstan-for-downgrade.neon'])
@@ -38,6 +34,4 @@ return RectorConfig::configure()
     // phpunit: true,
   )
 
-  ->withDowngradeSets(
-    php74: true,
-  );
+  ->withDowngradeSets(php74: true);
