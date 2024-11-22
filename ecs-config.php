6f4d958683b2cfd2759705da5bde3c577b09b425
Lars Gersmann <lars.gersmann@ionos.com>
2024-11-22 12:11:30 +0100

    wip : phpcs wordpress rules partly integrated

--- ecs-config.php
+++ ecs-config.php
@@ -1,17 +1,19 @@
 <?php
 
-declare(strict_types=1);
-
 // use Symplify\CodingStandard\Fixer\LineLength\LineLengthFixer;
+// use WordPressCS\WordPress\Sniffs\Arrays\ArrayDeclarationSpacingSniff;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 // use PhpCsFixer\Fixer\ArrayNotation\TrailingCommaInMultilineArrayFixer;
+// $codeSnifferConfig = new PHP_CodeSniffer\Config(["--standard=WordPress"]);
+// print_r( $codeSnifferConfig);
+// $codeSnifferRuleset = new PHP_CodeSniffer\Ruleset($codeSnifferConfig);
 
 return ECSConfig::configure()
   ->withPaths([__DIR__])
   ->withRootFiles()
   ->withSkip(
-    ['*/vendor/*', '*/build/*', '*/dist/*', '*/node_modules/*', '*/languages/*', '/wp-env-home/*', '/phpunit/*']
+    ['*/vendor/*', '*/build/*', '*/dist/*', '*/node_modules/*', '*/languages/*', '/wp-env-home/*', '/phpunit/*', '/tmp/*']
   )
   ->withPreparedSets(
     symplify: true,
@@ -30,13 +32,40 @@ return ECSConfig::configure()
   )
   // use 2 spaces instead of psr12 default (4 spaces)
   ->withSpacing(indentation: '  ')
-  // // force trailing commas in multiline arrays
-  // ->withRules([
-  //   TrailingCommaInMultilineArrayFixer::class,
-  // ]);
-/*
-  ->withConfiguredRule(LineLengthFixer::class, [
-    LineLengthFixer::LINE_LENGTH => 80,
-    LineLengthFixer::BREAK_LONG_LINES => true,
-  ])*/
+
+  /*
+    // example of adding a single rule without configuration
+    // force trailing commas in multiline arrays
+    ->withRules([
+      TrailingCommaInMultilineArrayFixer::class,
+    ]);
+  */
+
+  /*
+    // example of adding a single rule with configuration
+    ->withConfiguredRule(LineLengthFixer::class, [
+      LineLengthFixer::LINE_LENGTH => 80,
+      LineLengthFixer::BREAK_LONG_LINES => true,
+    ])
+  */
+
+  /*
+    // example of importing a WordPress Coding standard rule
+    ->withRules([ArrayDeclarationSpacingSniff::class])
+  */
+
+  /*
+    // example of importing a WordPress Coding standard rule with configuration
+
+    ->withConfiguredRule(ArrayDeclarationSpacingSniff::class, [])
+      // rulesWithConfiguration
+    ->withRules([
+      // our existing PSR12 set from PHP Code Sniffer
+      ...array_values($codeSnifferRuleset->sniffCodes),
+
+      // // and the two new rules I wanted from PHP CS Fixer
+      // PhpCsFixer\Fixer\PhpUnit\PhpUnitMethodCasingFixer::class,
+      // PhpCsFixer\Fixer\PhpUnit\PhpUnitTestAnnotationFixer::class,
+    ])
+  */
 ;
