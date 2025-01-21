7263c80e44f003f6148082a724c23fafcc2fd869
Thomas Rose <thomas.rose@ionos.com>
2025-01-21 07:20:55 +0000

    refactor

--- packages/wp-plugin/essentials/src/dashboard/blocks/deep-links/render.php
+++ packages/wp-plugin/essentials/src/dashboard/blocks/deep-links/render.php
@@ -1,12 +1,6 @@
 <?php
-/**
- * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
- */
-?>
 
-<?php
-
-$tenant = strtolower(get_option('ionos_group_brand_name', 'ionos'));
+$tenant = strtolower(get_option('ionos_group_brand_name', false));
 
 $config_file = __DIR__ . '/config/' . $tenant . '.php';
 
--- packages/wp-plugin/essentials/src/dashboard/blocks/deep-links/view.php
+++ packages/wp-plugin/essentials/src/dashboard/blocks/deep-links/view.php
@@ -1,13 +1,10 @@
-<div class="ionos-block-deep-links">
 <?php
 
-printf('<h3>%s</h3>', __('Deep-Links', 'ionos-essentials'));
-printf('<p>%s</p>', __('Use these links to get to your control panel.', 'ionos-essentials'));
+printf('<h3>%s</h3>', esc_html__('Deep-Links', 'ionos-essentials'));
+printf('<p>%s</p>', esc_html__('Use these links to get to your control panel.', 'ionos-essentials'));
 
 echo '<ul class="wp-block-list">';
 foreach ($links as $url => $anchor) {
-  printf('<li><a href="%s" target="_blank">%s</a></li>', $url, $anchor);
+  printf('<li><a href="%s" target="_blank">%s</a></li>', esc_url($url), esc_html($anchor));
 }
 echo '</ul>';
-?>
-</div>
