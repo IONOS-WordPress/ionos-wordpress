4c2e83dd03eb8d7fffab7c212f3b41a0a96c9823
Lars Gersmann <lars.gersmann@ionos.com>
2025-01-22 13:48:16 +0100

    feat: enhance e2e tests for deep links and add e2e-test-utils-playwright global setup for Playwright

--- packages/wp-plugin/essentials/inc/dashboard/tests/e2e/deep-links.spec.js
+++ packages/wp-plugin/essentials/inc/dashboard/tests/e2e/deep-links.spec.js
@@ -1,12 +1,13 @@
 import { test, expect } from '@wordpress/e2e-test-utils-playwright';
-import { execSync } from 'child_process';
+import { execSync } from 'node:child_process';
 
 test.describe('Deep Links in Dashboard', () => {
   test('Dashboard contains Deep Links for valid tenant', async ({ requestUtils, admin, page }) => {
     execSync('WP_ENV_HOME=./wp-env-home pnpm exec wp-env run tests-cli wp option update ionos_group_brand_name ionos');
-    page.goto('/custom_dashboard/ionos');
+    // visit the page rendered by essentails plugin into an iframe in wp admin
+    await admin.visitAdminPage('/?page=ionos-essentials-dashboard-hidden-admin-page-iframe&noheader=1&nofooter=');
 
-    const locator = page.locator('body');
+    const locator = await page.locator('body');
     await expect(locator).toHaveText(/Deep-Links/);
   });
 
@@ -14,9 +15,10 @@ test.describe('Deep Links in Dashboard', () => {
     execSync(
       'WP_ENV_HOME=./wp-env-home pnpm exec wp-env run tests-cli wp option update ionos_group_brand_name invalid_tenant'
     );
-    page.goto('/custom_dashboard/ionos');
+    // visit the page rendered by essentails plugin into an iframe in wp admin
+    await admin.visitAdminPage('/?page=ionos-essentials-dashboard-hidden-admin-page-iframe&noheader=1&nofooter=');
 
-    const locator = page.locator('body');
+    const locator = await page.locator('body');
     await expect(locator).not.toHaveText(/Deep-Links/);
   });
 });
