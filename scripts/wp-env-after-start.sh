0ee9c2f4b4ae5c609a7709c942c49e4646f87f46
Lars Gersmann <lars.gersmann@ionos.com>
2025-01-22 12:24:14 +0100

    fix: ensure a theme is activated in wp-env test instance

--- scripts/wp-env-after-start.sh
+++ scripts/wp-env-after-start.sh
@@ -198,6 +198,9 @@ for prefix in '' 'tests-' ; do
   # Update the page as front page by default.
   pnpm exec wp-env run ${prefix}cli wp option update show_on_front page
 
+  # activate twentytwenty-five theme in all wp-env instances (test and development)
+  pnpm exec wp-env run ${prefix}cli wp theme activate twentytwentyfive
+
   # emulate ionos brand by default
   pnpm exec wp-env run ${prefix}cli wp option update ionos_group_brand_name ionos
 done
