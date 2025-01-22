2cb0992109708e0c9951ca124090fda5bd20b9d3
Thomas Rose <thomas.rose@ionos.com>
2025-01-22 10:23:01 +0000

    introduce e2e tests for deep-links

---
+++ packages/wp-plugin/essentials/inc/dashboard/tests/e2e/deep-links.spec.js
@@ -0,0 +1,22 @@
+import { test, expect } from '@wordpress/e2e-test-utils-playwright';
+import { execSync } from 'child_process';
+
+test.describe('Deep Links in Dashboard', () => {
+  test('Dashboard contains Deep Links for valid tenant', async ({ requestUtils, admin, page }) => {
+    execSync('WP_ENV_HOME=./wp-env-home pnpm exec wp-env run tests-cli wp option update ionos_group_brand_name ionos');
+    page.goto('/custom_dashboard/ionos');
+
+    const locator = page.locator('body');
+    await expect(locator).toHaveText(/Deep-Links/);
+  });
+
+  test('Dashboard does not contain Deep Links for invalid tenant', async ({ requestUtils, admin, page }) => {
+    execSync(
+      'WP_ENV_HOME=./wp-env-home pnpm exec wp-env run tests-cli wp option update ionos_group_brand_name invalid_tenant'
+    );
+    page.goto('/custom_dashboard/ionos');
+
+    const locator = page.locator('body');
+    await expect(locator).not.toHaveText(/Deep-Links/);
+  });
+});
