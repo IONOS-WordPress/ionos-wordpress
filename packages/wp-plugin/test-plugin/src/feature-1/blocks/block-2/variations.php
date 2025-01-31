e7a9586b3bee644012a0a21fa53f6b1402b04bf2
Lars Gersmann <lars.gersmann@ionos.com>
2025-01-31 13:27:20 +0100

    fix: correct namespace declaration in block variations file

--- packages/wp-plugin/test-plugin/src/feature-1/blocks/block-2/variations.php
+++ packages/wp-plugin/test-plugin/src/feature-1/blocks/block-2/variations.php
@@ -1,7 +1,7 @@
 <?php
 
-namespace ionos_wordpress\test_plugin\feature_1\blocks\block_2;
-
-/**
+/*
  * @see https://make.wordpress.org/core/2024/10/20/miscellaneous-block-editor-changes-in-wordpress-6-7/#block-json-variations-field-can-now-be-set-to-name-of-php-file-that-generates-block-variations
  */
+
+namespace ionos_wordpress\test_plugin\feature_1\blocks\block_2;
