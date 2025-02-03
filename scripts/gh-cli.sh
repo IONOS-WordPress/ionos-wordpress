3888b56154bf574fe73428ff535a683908590afe
Lars Gersmann <lars.gersmann@ionos.com>
2025-02-03 12:16:30 +0100

    docs: enhance installation instructions for GitHub CLI in the script

--- scripts/gh-cli.sh
+++ scripts/gh-cli.sh
@@ -31,9 +31,19 @@ if ! command -v gh &> /dev/null; then
 
   cat <<'EOF'
 
-GitHub CLI (gh) is required to run this script. Script is automatically installed inm the DevContainer
+GitHub CLI (gh) is required to run this script. Script is automatically installed in the DevContainer
 
 If you really want to use it locally you can install it from https://cli.github.com/
+
+To install latest in Linux :
+
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+  && sudo mkdir -p -m 755 /etc/apt/keyrings \
+  && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+  && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+  && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+  && sudo apt update \
+  && sudo apt install gh entr -y
 EOF
   exit 1
 fi
