3ffa50beeb76609a2762fbb310640433a63c4309
Thomas Rose <thomas.rose@ionos.com>
2025-02-03 13:52:37 +0000

    set default brand

--- packages/wp-plugin/essentials/src/dashboard/blocks/deep-links/render.php
+++ packages/wp-plugin/essentials/src/dashboard/blocks/deep-links/render.php
@@ -1,10 +1,10 @@
 <?php
 
-$tenant = strtolower(\get_option('ionos_group_brand', false));
+$tenant = strtolower(\get_option('ionos_group_brand', 'ionos'));
 
 $config_file = __DIR__ . '/config/' . $tenant . '.php';
 
-if ($tenant && file_exists($config_file)) {
+if (file_exists($config_file)) {
   require($config_file);
 
   $market = strToLower(\get_option($tenant . '_market', 'de'));
