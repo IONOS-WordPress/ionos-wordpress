312d416e2792f0e22f395ba7bb27971a2a1343fc
Tobias <tobias.bode@ionos.com>
2025-02-21 15:17:36 +0100

    WIP
    Co-authored-by: Marko Feldmann <DerHerrFeldmann@users.noreply.github.com>

--- packages/wp-plugin/essentials/inc/dashboard/blocks/nba/index.php
+++ packages/wp-plugin/essentials/inc/dashboard/blocks/nba/index.php
@@ -1,16 +1,21 @@
 <?php
 
-namespace ionos_wordpress\essentials\dashboard;
+namespace ionos_wordpress\essentials\dashboard\blocks\next_best_actions;
 use const ionos_wordpress\essentials\PLUGIN_DIR;
 
-require_once __DIR__ . '/model.php';
-
 \add_action('init', function () {
-  \register_block_type(PLUGIN_DIR . '/build/dashboard/blocks/next-best-actions');
+  \register_block_type(
+    PLUGIN_DIR . '/build/dashboard/blocks/next-best-actions',
+  );
 });
-
+    // [
+    //   'render_callback' => function ($attributes) {
+    //     require_once __DIR__ . '/render.php';
+    //   },
+    // ]
 \add_action('admin_init', function () {
   if (isset($_GET['complete_nba'])) {
+    require_once __DIR__ . '/model.php';
     $nba_id = $_GET['complete_nba'];
 
     $nba = \ionos_wordpress\essentials\dashboard\blocks\next_best_actions\model\NBA::getNBA($nba_id);
@@ -19,14 +24,15 @@ require_once __DIR__ . '/model.php';
 });
 
 \add_action('rest_api_init', function () {
-  \register_rest_route('ionos/v1', '/complete_nba/(?P<id>[a-zA-Z0-9-]+)', [
+  \register_rest_route('ionos/v1', '/dismiss_nba/(?P<id>[a-zA-Z0-9-]+)', [
     'methods' => 'GET',
     'callback' => function ($request) {
+      require_once __DIR__ . '/model.php';
 			$params = $request->get_params();
 			$nba_id = $params['id'];
 
       $nba = \ionos_wordpress\essentials\dashboard\blocks\next_best_actions\model\NBA::getNBA($nba_id);
-      $res = $nba->setStatus( "completed", true);
+      $res = $nba->setStatus( "dismissed", true);
       if ($res) {
         return new \WP_REST_Response(['status' => 'success', 'res' => $res], 200);
       }
--- packages/wp-plugin/essentials/inc/dashboard/blocks/nba/render.php
+++ packages/wp-plugin/essentials/inc/dashboard/blocks/nba/render.php
@@ -5,19 +5,39 @@ namespace ionos_wordpress\essentials\dashboard\blocks\next_best_actions;
 use ionos_wordpress\essentials\dashboard\blocks\next_best_actions\model\NBA;
 
 require_once __DIR__ . '/model.php';
+$actions = NBA::getActions();
+if (empty($actions)) {
+  return;
+}
 
-printf('<h3>%s</h3>', \esc_html__('Next best actions ⚡', 'ionos-essentials'));
-echo '<div class="wp-block-list">';
-foreach (NBA::getActions() as $action) {
-  $completed = $action->completed;
+echo '<div class="wp-block-group alignwide">';
+// headline
+echo '<div class="wp-block-group">';
+printf('<h2 class="wp-block-heading">%s</h2>', \esc_html__('Next best actions ⚡', 'ionos-essentials'));
+printf('<p>%s</p>', \esc_html__('Description of this block', 'ionos-essentials'));
+echo '</div>';
+echo '<div class="wp-block-columns">';
+
+
+foreach ($actions as $action) {
+  $active = $action->active;
+  if (! $active) {
+    continue;
+  }
+  echo '<div class="wp-block-column is-style-default has-border-color">';
+  printf('<h2 class="wp-block-heading">%s</h2>', \esc_html($action->title, 'ionos-essentials'));
+  printf('<p>%s</p>', \esc_html($action->title, 'ionos-essentials'));
+
   printf(
     '<p>%s<a href="%s" target="_top">%s</a>%s</p>%s',
-    $completed ? '<s>' : '',
+    $active ? '' : '<s>',
     \esc_url($action->link),
     \esc_html($action->title),
-    $completed ? '</s>' : '',
-    $completed ? '' : "<button id='{$action->id}' class='dismiss-nba'>dismiss</button>",
+    $active ? '' : '</s>',
+    $active ? "<button id='{$action->id}' class='dismiss-nba'>dismiss</button>" : "",
   );
+  echo '</div>';
 }
-echo '</div>';
 
+echo '</div>';
+echo '</div>';
