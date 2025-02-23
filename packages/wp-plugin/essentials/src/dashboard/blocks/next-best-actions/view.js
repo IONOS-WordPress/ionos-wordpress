9abecbb950322e33e5817ee4627537c013c4f105
Tobias Bode <tobias.bode@ionos.com>
2025-02-23 12:42:16 +0100

    WIP: dynamic part. hide dismissed actions

--- packages/wp-plugin/essentials/src/dashboard/blocks/next-best-actions/view.js
+++ packages/wp-plugin/essentials/src/dashboard/blocks/next-best-actions/view.js
@@ -2,12 +2,16 @@ import apiFetch from '@wordpress/api-fetch';
 
 console.log('Next Best Actions block loaded');
 document.querySelectorAll('.dismiss-nba').forEach((el) => {
-  el.addEventListener('click', (click) => {
+  el.addEventListener('click', async (click) => {
     console.log(wp.ajax);
     // alert(`Dismiss NBA ${click.target.id}`);
-    const res = apiFetch({
+    const res = await apiFetch({
       path: `ionos/v1/dismiss_nba/${click.target.id}`,
       method: 'GET',
     });
+    if (res.status === 'success') {
+      // TODO
+      click.target.parentElement.parentElement.parentElement.parentElement.style.display = 'none';
+    }
   });
 });
