d1b41bbcd12a4eb992e91e36ccb4535fd8832d6e
DerHerrFeldmann <markofeldmann.development@gmail.com>
2025-02-27 16:59:44 +0100

    update readability

--- packages/wp-plugin/essentials/inc/dashboard/blocks/nba/render.php
+++ packages/wp-plugin/essentials/inc/dashboard/blocks/nba/render.php
@@ -7,41 +7,44 @@ if (empty($actions)) {
   return;
 }
 
-$template = '<div id="ionos-dashboard__essentials_nba" class="wp-block-group alignwide">'
-. '<div class="wp-block-group is-vertical is-content-justification-left is-layout-flex wp-container-core-group-is-layout-2 wp-block-group-is-layout-flex">'
-. sprintf('<h2 class="wp-block-heading">%s</h2>', \esc_html__('Next best actions ⚡', 'ionos-essentials'))
-. sprintf('<p>%s</p>', \esc_html__('Description of this block', 'ionos-essentials'))
-. '</div>'
-. '<div class="wp-block-columns is-layout-flex wp-container-core-columns-is-layout-1 wp-block-columns-is-layout-flex">'
-. '%s'
-. '</div></div>';
+$template = '
+<div id="ionos-dashboard__essentials_nba" class="wp-block-group alignwide">
+    <div class="wp-block-group is-vertical is-content-justification-left is-layout-flex wp-container-core-group-is-layout-2 wp-block-group-is-layout-flex">
+        <h2 class="wp-block-heading">%s</h2>
+        <p>%s</p>
+    </div>
+    <div class="wp-block-columns is-layout-flex wp-container-core-columns-is-layout-1 wp-block-columns-is-layout-flex">
+        %s
+    </div>
+</div>';
+
+$header = \esc_html__('Next best actions ⚡', 'ionos-essentials');
+$description = \esc_html__('Description of this block', 'ionos-essentials');
 
 $body = '';
 foreach ($actions as $action) {
-  $active = $action->active;
-  if (! $active) {
+  if (!$action->active) {
     continue;
   }
-  $body .= '<div class="wp-block-column is-style-default has-border-color is-layout-flow wp-block-column-is-layout-flow">'
-  . sprintf('<h2 class="wp-block-heading">%s</h2>', \esc_html($action->title, 'ionos-essentials'))
-  . sprintf('<p>%s</p>', \esc_html($action->description, 'ionos-essentials'))
-  . '<div class="wp-block-buttons is-layout-flex wp-block-buttons-is-layout-flex">'
-  . sprintf(
-    '<div class="wp-block-button"><a href="%s" class="wp-block-button__link wp-element-button" target="_top">%s</a></div>',
-    \esc_url($action->link),
-    \esc_html("Primary button", 'ionos-essentials'),
-  )
-  . '<div class="wp-block-button is-style-outline is-style-outline--1">'
-  . sprintf(
-    '<div class="wp-block-button"><a id="%s" class="wp-block-button__link wp-element-button dismiss-nba" target="_top">%s</a></div>',
-    $action->id,
-    \esc_html("Dismiss", 'ionos-essentials')
-  )
-  . '</div></div></div>';
+
+  $target = strpos(\esc_url($action->link), home_url()) === false ? '_blank' : '_top';
+  $body .= '
+    <div class="wp-block-column is-style-default has-border-color is-layout-flow wp-block-column-is-layout-flow">
+        <h2 class="wp-block-heading">' . \esc_html($action->title, 'ionos-essentials') . '</h2>
+        <p>' . \esc_html($action->description, 'ionos-essentials') . '</p>
+        <div class="wp-block-buttons is-layout-flex wp-block-buttons-is-layout-flex">
+            <div class="wp-block-button">
+                <a href="' . \esc_url($action->link) . '" class="wp-block-button__link wp-element-button" target="' . $target . '">' . \esc_html("Primary button", 'ionos-essentials') . '</a>
+            </div>
+            <div class="wp-block-button is-style-outline is-style-outline--1">
+                <a id="' . $action->id . '" class="wp-block-button__link wp-element-button dismiss-nba" target="_top">' . \esc_html("Dismiss", 'ionos-essentials') . '</a>
+            </div>
+        </div>
+    </div>';
 }
 
 if (empty($body)) {
   return;
 }
 
-\printf($template, $body);
+\printf($template, $header, $description, $body);
