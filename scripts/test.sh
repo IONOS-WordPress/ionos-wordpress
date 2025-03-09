3a10d476e80b1b51ed637c033d1f4a4f9c98b270
Lars Gersmann <lars.gersmann@ionos.com>
2025-03-09 07:32:53 +0100

    fix: remove redundant function for Playwright environment preparation

--- scripts/test.sh
+++ scripts/test.sh
@@ -58,14 +58,12 @@ done
 # invoke all tests by default
 [[ ${#USE[@]} -eq 0 ]] && USE=("all")
 
-function ionos.wordpress.prepare_playwright_environment() {
-  ionos.wordpress.log_info "found playwight installations : $(find ~/.cache/ms-playwright -path "*/chrome-linux/chrome" 2>/dev/null | wc -l)"
-  ionos.wordpress.log_info "$(find ~/.cache/ms-playwright -path "*/chrome-linux/chrome" 2>/dev/null)"
-}
-
 # ensure the playwright cache is generated in the same environment (devcontainer or local) as the tests are executed
 # (this is necessary because the cache is not portable between environments)
 if [[ "${USE[@]}" =~ e2e|react ]]; then
+  ionos.wordpress.log_info "found playwight installations : $(find ~/.cache/ms-playwright -path "*/chrome-linux/chrome" 2>/dev/null | wc -l)"
+  ionos.wordpress.log_info "$(find ~/.cache/ms-playwright -path "*/chrome-linux/chrome" 2>/dev/null)"
+
   PLAYWRIGHT_DIR=$(realpath ./playwright)
   if [[ -f "$PLAYWRIGHT_DIR/.cache/metainfo.json" ]] && ! grep "$PLAYWRIGHT_DIR" ./playwright/.cache/metainfo.json > /dev/null; then
     # ./playwright/.cache/metainfo.json contains not the absolute path to the cache directory of the current environment
@@ -76,8 +74,6 @@ fi
 
 if [[ "${USE[@]}" =~ all|react ]]; then
   (
-    ionos.wordpress.prepare_playwright_environment
-
     # execute playwright tests. provide part specific options and all positional arguments that are jsx files
     pnpm exec playwright test --pass-with-no-tests -c ./playwright-ct.config.js \
       "${USE_OPTIONS[react]:---quiet}" \
@@ -108,7 +104,6 @@ fi
 if [[ "${USE[@]}" =~ all|e2e ]]; then
   # start wp-env e2e tests. provide part specific options and all positional arguments that are php files
   (
-    ionos.wordpress.prepare_playwright_environment
     # pnpm exec wp-scripts test-playwright --pass-with-no-tests -c ./playwright.config.js \
     pnpm exec playwright test --pass-with-no-tests -c ./playwright.config.js \
       "${USE_OPTIONS[e2e]:---quiet}" \
