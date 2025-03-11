e72f3f5070887933991fe2d1b4d4a8aa711398ea
DerHerrFeldmann <markofeldmann.development@gmail.com>
2025-03-11 10:36:13 +0100

    fix linting issues

--- packages/wp-plugin/essentials/inc/switch-page/index.php
+++ packages/wp-plugin/essentials/inc/switch-page/index.php
@@ -13,16 +13,18 @@ if (! defined('ABSPATH')) {
  * Helper function for wp_option 'ionos_group_brand_menu'.
  * Used in visible stuff to the user.
  */
-function get_brand_menu() {
-  return \get_option( 'ionos_group_brand_menu', 'IONOS' );
+function get_brand_menu()
+{
+  return \get_option('ionos_group_brand_menu', 'IONOS');
 }
 
 /**
  * Helper function for wp_option 'ionos_group_brand'.
  * Used in urls and internal stuff.
  */
-function get_brand_lowercase() {
-  return strtolower( \get_option( 'ionos_group_brand', 'ionos' ) );
+function get_brand_lowercase()
+{
+  return strtolower(\get_option('ionos_group_brand', 'ionos'));
 }
 /**
  * Add onboarding menu page.
@@ -37,7 +39,7 @@ function get_brand_lowercase() {
     'Assistant',
     'manage_options',
     get_brand_lowercase() . '-onboarding',
-    fn () => \load_template( __DIR__ . '/view.php' )
+    fn () => \load_template(__DIR__ . '/view.php')
   )
 );
 
@@ -47,22 +49,24 @@ function get_brand_lowercase() {
  */
 \add_filter(
   'wp_redirect',
-  function ( $location ) {
-    if ( $location === \admin_url( 'admin.php?page=extendify-launch' ) ) {
-      return \admin_url( 'admin.php?page=' . get_brand_lowercase() . '-onboarding' );
-    } else if ( $location === \admin_url( 'admin.php?page=extendify-assist' ) ) {
-      return \admin_url( 'admin.php?page=' . get_brand_lowercase() . '-essentials-dashboard' );
+  function ($location) {
+    if ($location === \admin_url('admin.php?page=extendify-launch')) {
+      return \admin_url('admin.php?page=' . get_brand_lowercase() . '-onboarding');
+    } elseif ($location === \admin_url('admin.php?page=extendify-assist')) {
+      return \admin_url('admin.php?page=' . get_brand_lowercase() . '-essentials-dashboard');
     }
 
     return $location;
-  }, 10000, 1
+  },
+  10000,
+  1
 );
 
 \add_action(
   'load-toplevel_page_extendify-assist',
   function () {
-    if ( isset( $_GET['extendify-launch-success'] )) {
-      \wp_safe_redirect( \admin_url() . 'admin.php?page=' . get_brand_lowercase() . '-essentials-dashboard' );
+    if (isset($_GET['extendify-launch-success'])) {
+      \wp_safe_redirect(\admin_url() . 'admin.php?page=' . get_brand_lowercase() . '-essentials-dashboard');
       exit;
     }
   }
@@ -70,29 +74,27 @@ function get_brand_lowercase() {
 
 \add_filter(
   'admin_title',
-  function ( $title ) {
+  function ($title) {
     // phpcs:ignore WordPress.Security.NonceVerification.Recommended
-    if ( isset( $_GET['page'] ) && $_GET['page'] === get_brand_lowercase() . '-onboarding' ) {
-      return get_brand_menu() . ' ' . __( 'Onboarding', 'ionos-assistant' );
+    if (isset($_GET['page']) && $_GET['page'] === get_brand_lowercase() . '-onboarding') {
+      return get_brand_menu() . ' ' . __('Onboarding', 'ionos-assistant');
     }
     return $title;
   },
   11
 );
 
-
 \add_action(
   'admin_enqueue_scripts',
-  function ( $hook_suffix ) {
-    if ( ! str_contains( $hook_suffix, '_page_' . get_brand_lowercase() . '-onboarding' ) ) {
+  function ($hook_suffix) {
+    if (! str_contains($hook_suffix, '_page_' . get_brand_lowercase() . '-onboarding')) {
       return;
     }
     \wp_enqueue_style(
       'ionos-assistant-switch-page',
-      \plugins_url( 'style.css', __FILE__ ),
+      \plugins_url('style.css', __FILE__),
       [],
-      filemtime( PLUGIN_DIR . '/inc/switch-page/style.css' )
+      filemtime(PLUGIN_DIR . '/inc/switch-page/style.css')
     );
   }
 );
-
