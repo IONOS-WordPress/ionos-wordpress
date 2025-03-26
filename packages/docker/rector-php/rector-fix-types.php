f66bb2e711dbb03c70147093777de7270fd313e8
Lars Gersmann <lars.gersmann@ionos.com>
2025-03-26 15:33:34 +0100

    refactor: linted all sources files

--- packages/docker/rector-php/rector-fix-types.php
+++ packages/docker/rector-php/rector-fix-types.php
@@ -3,14 +3,10 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-
 use Rector\ValueObject\PhpVersion;
 
-return RectorConfig::configure()
-  ->withSkip([__DIR__ . '/dist/vendor', __DIR__ . '/dist/languages'])
-  ->withParallel()
-  ->withPhpVersion(PhpVersion::PHP_83)
-  ->withPreparedSets(
+return RectorConfig::configure()->withSkip([__DIR__ . '/dist/vendor', __DIR__ . '/dist/languages'])->withParallel()
+  ->withPhpVersion(PhpVersion::PHP_83)->withPreparedSets(
     // deadCode : true,
     // codeQuality : true,
     codingStyle : true,
--- packages/wp-plugin/essentials/inc/dashboard/editor.php
+++ packages/wp-plugin/essentials/inc/dashboard/editor.php
@@ -21,20 +21,20 @@ const DASHBOARD_POST_TITLE = 'Custom IONOS Dashboard';
     POST_TYPE_SLUG,
     [
       'labels' => [
-        'name' => 'IONOS Dashboards',
+        'name'          => 'IONOS Dashboards',
         'singular_name' => 'IONOS Dashboard',
       ],
-      'public' => true,
+      'public'             => true,
       'publicly_queryable' => true,
-      'show_in_menu' => 'options-general.php',
-      'show_in_admin_bar' => true,
-      'show_in_rest' => true,
-      'supports' => ['title', 'editor'],
+      'show_in_menu'       => 'options-general.php',
+      'show_in_admin_bar'  => true,
+      'show_in_rest'       => true,
+      'supports'           => ['title', 'editor'],
     ]
   );
 
   \register_block_template('ionos-dashboard-page//' . POST_TYPE_TEMPLATE_SLUG, [
-    'title' => POST_TYPE_TEMPLATE_SLUG,
+    'title'   => POST_TYPE_TEMPLATE_SLUG,
     'content' => (function () {
       ob_start();
       require_once(__DIR__ . '/data/block-template.php');
@@ -53,7 +53,7 @@ const DASHBOARD_POST_TITLE = 'Custom IONOS Dashboard';
   // create dashboard posts for all saved dashboards
   $ionos_dashboard_names = array_column(
     \get_posts([
-      'post_type' => POST_TYPE_SLUG,
+      'post_type'      => POST_TYPE_SLUG,
       'posts_per_page' => -1,
     ]),
     'post_name'
@@ -66,12 +66,12 @@ const DASHBOARD_POST_TITLE = 'Custom IONOS Dashboard';
       continue;
     }
     \wp_insert_post([
-      'post_title' => $name,
-      'post_name' => $name,
+      'post_title'   => $name,
+      'post_name'    => $name,
       'post_content' => file_get_contents($dir . '/post_content.html'),
-      'post_status' => 'publish',
-      'post_author' => 1,
-      'post_type' => POST_TYPE_SLUG,
+      'post_status'  => 'publish',
+      'post_author'  => 1,
+      'post_type'    => POST_TYPE_SLUG,
       // 'page_template' => POST_TYPE_TEMPLATE_SLUG // templates registered by plugins are not supported yet (as of WP 6.7)
     ]);
   }
@@ -213,8 +213,8 @@ function _persist_dashboard(\WP_Post $post): void
     foreach ($queued_styles as $style) {
       $css_url = $style->src;
       if ($css_url) {
-        $style->src = false;
-        $css_file = \wp_parse_url($css_url, PHP_URL_PATH);
+        $style->src  = false;
+        $css_file    = \wp_parse_url($css_url, PHP_URL_PATH);
         $css_content = file_get_contents(\wp_normalize_path(ABSPATH . $css_file));
         \wp_add_inline_style($style->handle, $css_content);
       }
@@ -239,11 +239,11 @@ function _persist_dashboard(\WP_Post $post): void
   callback: function ($data, $postarr, $unsanitized_postarr, $update) {
     if (file_exists(GLOBAL_STYLES_FILE) && 'wp_global_styles' === $data['post_type'] && ! $update) {
       // post_content can be slashed and is expected slashed after the filter
-      $post_content = json_decode(wp_unslash($data['post_content']), true);
+      $post_content   = json_decode(wp_unslash($data['post_content']), true);
       $data_from_file = \wp_json_file_decode(GLOBAL_STYLES_FILE, [
         'associative' => true,
       ]);
-      $new_data = array_merge($post_content, $data_from_file);
+      $new_data             = array_merge($post_content, $data_from_file);
       $data['post_content'] = wp_slash(\wp_json_encode($new_data));
     }
     return $data;
