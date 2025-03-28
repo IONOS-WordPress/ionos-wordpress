51e9d633ea354560165a88466722f17c317c9722
DerHerrFeldmann <markofeldmann.development@gmail.com>
2025-03-28 12:17:11 +0100

    feat: redesign welcome block with improved layout and styling

--- packages/wp-plugin/essentials/inc/dashboard/blocks/welcome/index.php
+++ packages/wp-plugin/essentials/inc/dashboard/blocks/welcome/index.php
@@ -3,7 +3,6 @@
 namespace ionos_wordpress\essentials\dashboard\blocks\welcome;
 
 use const ionos_wordpress\essentials\PLUGIN_DIR;
-use const ionos_wordpress\essentials\PLUGIN_FILE;
 
 \add_action('init', function () {
   \register_block_type(
@@ -16,52 +15,34 @@ use const ionos_wordpress\essentials\PLUGIN_FILE;
 
 function render_callback(): string
 {
-  $template = '<div id="essentials-welcome_block">
-<div class="card" style="max-width: 640px;">
-<div class="card__content">
-    <section class="card__section">
-      <h2 class="card__headline">Welcome to your [Brand] Hub</h2>
-      <h4 class="card__subheadline">This overview is your gateway to unlocking the full potential of your WordPress website.</h4>
-      <ul class="check-list">
-        <li>Recommendations for next steps</li>
-        <li>Helpful links and shortcuts</li>
-        <li>Comprehensive help section, including: AI chat support, Guided tours and an extensive knowledge database</li>
-      </ul>
-      <div class="paragraph">Stay tuned for exciting new features and updates to come!</div>
-    </section>
-    <footer class="card__footer card__footer--align-center">
-      <a class="button button--primary">Order now</a>
-    </footer>
-  </div>
-  </div>
-</div>';
-  // return $template;
-
-
-$message_version = "1.0.0";
-
-  return "<dialog>
-<div class='card' style='max-width: 640px;'>
-<div class='card__content'>
-    <section class='card__section'>
-      <h2 class='card__headline'>" . sprintf(\__('Welcome to your %s Hub', 'ionos-essentials'), \get_option('ionos_group_brand_menu', 'IONOS'))  ."</h2>
-      <h4 class='card__subheadline'>" . \__(
-  'This overview is your gateway to unlocking the full potential of your WordPress website.',
-  'ionos-essentials'
-)."</h4>
-      <ul class='check-list'>
-        <li>".\__('Recommendations for next steps', 'ionos-essentials')."</li>
-        <li>".\__('Helpful links and shortcuts', 'ionos-essentials')."</li>
-        <li>".\__(
-  'Comprehensive help section, including: AI chat support, Guided tours and an extensive knowledge database',
-  'ionos-essentials')."</li>
-      </ul>
-      <div class='paragraph'>".\__('Stay tuned for exciting new features and updates to come!', 'ionos-essentials')."</div>
-    </section>
-    <footer class='card__footer card__footer--align-center'>
-      <button class='button button--primary' data-version='{$message_version}' autofocus >" .\__('Close', 'ionos-essentials') . "</button>
-    </footer>
-  </div>
-  </div>
-  </dialog>";
+  $message_version = '1.0.0';
+  return "<dialog id='essentials-welcome_block'>
+    <div class='horizontal-card'>
+      <header class='horizontal-card__header'>
+      <img class='horizontal-card__visual' src='" . \plugins_url(
+    'data/tenant-logos/welcome-banner.png',
+    \dirname(__DIR__, 1)
+  ) . "' alt='Welcome Banner'>      </header>
+      <div class='horizontal-card__content'>
+        <section class='horizontal-card__section'>
+          <h2 class='horizontal-card__headline'>" . sprintf(
+    \__('Welcome to your %s Hub', 'ionos-essentials'),
+    \get_option('ionos_group_brand_menu', 'IONOS')
+  ) . "</h2>
+          <p class='paragraph'>" . \__('This overview is your gateway to unlocking the full potential of your WordPress website.', 'ionos-essentials') . "</p>
+        </section>
+        <section class='horizontal-card__section'>
+          <ul class='check-list'>
+            <li>" . \__('Recommendations for next steps', 'ionos-essentials') . '</li>
+            <li>' . \__('Helpful links and shortcuts', 'ionos-essentials') . '</li>
+            <li>' . \__('Comprehensive help section, including: AI chat support, Guided tours and an extensive knowledge database', 'ionos-essentials') . "</li>
+          </ul>
+          <div class='paragraph'>" . \__('Stay tuned for exciting new features and updates to come!', 'ionos-essentials') . "</div>
+        </section>
+      </div>
+    </div>
+        <footer class='horizontal-card__footer horizontal-card__footer--small-align-center'>
+          <button class='button button--primary' data-version='{$message_version}' autofocus >" . \__('Close', 'ionos-essentials') . '</button>
+        </footer>
+  </dialog>';
 }
--- packages/wp-plugin/essentials/src/dashboard/blocks/welcome/view.js
+++ packages/wp-plugin/essentials/src/dashboard/blocks/welcome/view.js
@@ -8,10 +8,13 @@ closeButton.addEventListener('click', () => {
   dialog.close();
 });
 
-dialog.addEventListener('click', function(event) {
+dialog.addEventListener('click', function (event) {
   var rect = dialog.getBoundingClientRect();
-  var isInDialog = (rect.top <= event.clientY && event.clientY <= rect.top + rect.height &&
-    rect.left <= event.clientX && event.clientX <= rect.left + rect.width);
+  var isInDialog =
+    rect.top <= event.clientY &&
+    event.clientY <= rect.top + rect.height &&
+    rect.left <= event.clientX &&
+    event.clientX <= rect.left + rect.width;
   if (!isInDialog) {
     dialog.close();
   }
