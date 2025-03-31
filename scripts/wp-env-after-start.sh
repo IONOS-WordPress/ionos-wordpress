f081b3a156084101edf41e42544da46a9b198264
Lars Gersmann <lars.gersmann@ionos.com>
2025-03-31 16:09:59 +0200

    refactor: use absolute paths for phpunit file copying in wp-env-after-start.sh

--- scripts/wp-env-after-start.sh
+++ scripts/wp-env-after-start.sh
@@ -34,11 +34,11 @@ EOF
   # MARK: copy PHPUNIT
   # copy phpunit files from wp-env container to phpunit-wordpress
   WORDPRESS_TEST_CONTAINER=$(docker ps -q --filter "name=tests-wordpress")
-  docker cp "$WORDPRESS_TEST_CONTAINER:/home/$USER/.composer/vendor/" ./phpunit/
+  docker cp "$WORDPRESS_TEST_CONTAINER:/home/$USER/.composer/vendor/" "$(pwd)/phpunit/"
 
   # copy our phpunit config and bootstrap file to the wp-env wordpress test instance instead of mapping them in wp-env.json
-  docker cp ./phpunit/phpunit.xml "$WORDPRESS_TEST_CONTAINER:/var/www/html"
-  docker cp ./phpunit/bootstrap.php "$WORDPRESS_TEST_CONTAINER:/var/www/html"
+  docker cp "$(pwd)/phpunit/phpunit.xml" "$WORDPRESS_TEST_CONTAINER:/var/www/html"
+  docker cp "$(pwd)/phpunit/bootstrap.php" "$WORDPRESS_TEST_CONTAINER:/var/www/html"
 
   # ENDMARK
 
