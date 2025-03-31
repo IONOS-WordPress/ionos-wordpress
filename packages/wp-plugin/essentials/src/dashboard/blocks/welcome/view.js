b70b9304906b1dd2e044ad572e112f03aeeb4d77
DerHerrFeldmann <markofeldmann.development@gmail.com>
2025-03-31 22:08:34 +0200

    feat: implement welcome dialog with user meta persistence and nonce verification

--- packages/wp-plugin/essentials/src/dashboard/blocks/welcome/view.js
+++ packages/wp-plugin/essentials/src/dashboard/blocks/welcome/view.js
@@ -1,9 +1,15 @@
+import apiFetch from '@wordpress/api-fetch';
+
 ((dialog) => {
   if (!dialog) {
     return;
   }
   dialog.showModal();
-  dialog.querySelector('button').onclick = () => dialog.close();
+  dialog.querySelector('button').onclick = (event) => {
+    event.preventDefault();
+    persistDialog(event.target?.getAttribute('nonce'));
+    dialog.close();
+  }
   dialog.onclick = (event) => {
     const rect = dialog.getBoundingClientRect();
     const isInDialog =
@@ -15,4 +21,17 @@
       dialog.close();
     }
   };
+
+  const persistDialog = ($nonce = 'mops') => {
+    apiFetch({
+      path: 'ionos/essentials/dashboard/welcome/v1/closer',
+      method: 'POST',
+      data: {
+        nonce: $nonce,
+      },
+    }).then((response) => {
+      console.log('Dialog closed:', response);
+    });
+  };
+
 })(document.querySelector('dialog'));
