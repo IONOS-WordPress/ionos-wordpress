e0b25e07a9aad365559e2d01f889611ef7135a39
Tobias <tobias.bode@ionos.com>
2025-03-31 12:56:18 +0200

    code review

--- packages/wp-plugin/essentials/src/dashboard/blocks/welcome/view.js
+++ packages/wp-plugin/essentials/src/dashboard/blocks/welcome/view.js
@@ -1,21 +1,19 @@
-const dialog = document.querySelector('dialog');
-const closeButton = document.querySelector('dialog button');
-
-dialog.showModal();
-
-// "Close" button closes the dialog
-closeButton.addEventListener('click', () => {
-  dialog.close();
-});
-
-dialog.addEventListener('click', function (event) {
-  var rect = dialog.getBoundingClientRect();
-  var isInDialog =
-    rect.top <= event.clientY &&
-    event.clientY <= rect.top + rect.height &&
-    rect.left <= event.clientX &&
-    event.clientX <= rect.left + rect.width;
-  if (!isInDialog) {
-    dialog.close();
+((dialog) => {
+  if (!dialog) {
+    return;
   }
-});
+  dialog.showModal();
+  // "Close" button closes the dialog
+  dialog.querySelector('button').addEventListener('click', () => dialog.close());
+  dialog.addEventListener('click', function (event) {
+    var rect = dialog.getBoundingClientRect();
+    var isInDialog =
+      rect.top <= event.clientY &&
+      event.clientY <= rect.top + rect.height &&
+      rect.left <= event.clientX &&
+      event.clientX <= rect.left + rect.width;
+    if (!isInDialog) {
+      dialog.close();
+    }
+  });
+})(document.querySelector('dialog'));
