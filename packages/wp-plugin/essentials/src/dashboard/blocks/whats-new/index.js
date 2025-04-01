01225681f7191694b4cc1e619b5769051a8465d8
Tobi Bode <12033092+tobibode@users.noreply.github.com>
2025-04-01 14:31:11 +0200

    feat(whats-new): create translatable but yet unstyled block

---
+++ packages/wp-plugin/essentials/src/dashboard/blocks/whats-new/index.js
@@ -0,0 +1,9 @@
+import { registerBlockType } from '@wordpress/blocks';
+import Edit from './edit.js';
+import metadata from './block.json';
+import './style.scss';
+
+registerBlockType(metadata.name, {
+  edit: Edit,
+  save: () => null, // server-side rendering doesnt need a save function
+});
