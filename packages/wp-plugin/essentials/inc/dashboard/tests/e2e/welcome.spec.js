4e003dd125c66d2f8e4b89815d8faf52c1110783
Marko Feldmann <46565390+DerHerrFeldmann@users.noreply.github.com>
2025-04-01 16:58:08 +0200

    feature/GPHWPP-3750_welcome-message-popup (#194)
    * feat: add welcome block with server-side rendering and styles
    
    * feat: enhance welcome block with localization support and improved dialog functionality
    
    * feat: redesign welcome block with improved layout and styling
    
    * tests(nba): close dialog if open
    
    * change proposed message versioning
    
    * code review
    
    * code review
    
    * code review
    
    * rendered skeleton
    
    * feat: implement welcome dialog with user meta persistence and improved styling
    
    * feat: implement welcome dialog with user meta persistence and nonce verification
    
    * reduce image size by 90%
    
    * fix:  adjust translations in welcome block
    
    * fix: minor text change in nba
    
    * quick links fix + lint
    
    * fix: update user meta key for welcome block and add e2e tests for welcome dialog functionality
    
    * feat: replace welcome banner image with PNG and adjust styles for better layout
    
    * fix: adjust margin for folded dashboard layout
    
    * code review improvements
    
    ---------
    
    Co-authored-by: Tobias Bode <tobias.bode@ionos.com>
    Co-authored-by: Tobi Bode <12033092+tobibode@users.noreply.github.com>

---
+++ packages/wp-plugin/essentials/inc/dashboard/tests/e2e/welcome.spec.js
@@ -0,0 +1,38 @@
+import { test, expect } from '@wordpress/e2e-test-utils-playwright';
+import { execSync } from 'node:child_process';
+
+test.describe('essentials:dashboard next-best-actions block', () => {
+  test.beforeAll(async () => {
+    try {
+      await execSync('pnpm -s wp-env run tests-cli wp rewrite structure /%postname% --hard');
+      await execSync('pnpm -s wp-env run tests-cli wp user meta delete 1 ionos_essentials_welcome');
+    } catch (error) {}
+  });
+
+  test('test welcome banner', async ({ admin, page }) => {
+    // show dashboard and click on dismiss button of "create-page" action
+    await admin.visitAdminPage('/');
+    // get the iframe element
+    let iframeLocator = await page.locator('iframe');
+    // get the iframe's body element
+    let iframeBodyLocator = await iframeLocator.contentFrame().locator('body');
+
+    let dialogBox = await iframeBodyLocator.locator('#essentials-welcome_block');
+    await expect(dialogBox).toHaveCount(1);
+    await expect(dialogBox).toHaveAttribute('open');
+
+    await dialogBox.locator('button').click();
+    await expect(dialogBox).not.toHaveAttribute('open');
+  });
+
+  test('test welcome is still closed', async ({ admin, page }) => {
+    // show dashboard and click on dismiss button of "create-page" action
+    await admin.visitAdminPage('/');
+
+    let iframeLocator = await page.locator('iframe');
+    let iframeBodyLocator = await iframeLocator.contentFrame().locator('body');
+
+    let dialogBox = await iframeBodyLocator.locator('#essentials-welcome_block');
+    await expect(dialogBox).toHaveCount(0);
+  });
+});
