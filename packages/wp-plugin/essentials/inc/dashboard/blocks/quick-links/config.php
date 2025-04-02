721c271eb59ec5cdfdf20ee1cd3b6792cf9dd592
peperuda-dev <denise.stamatov@ionos.com>
2025-04-02 14:12:35 +0200

    add functionality for showing and hiding nba-actions (#195)
    * add (show-only-six): functionality init load and hide remaining
    * add (show-only-six): loading after dismissing
    * add (show-only-six): include dashboard nba css
    * add (show-only-six): update view.js
    * add (show-only-six): update nba-test

--- packages/wp-plugin/essentials/inc/dashboard/blocks/quick-links/config.php
+++ packages/wp-plugin/essentials/inc/dashboard/blocks/quick-links/config.php
@@ -5,35 +5,35 @@ namespace ionos_wordpress\essentials\dashboard\blocks\quick_links;
 $blog_url = \get_bloginfo('url');
 
 $links = [
-  ['url' => $blog_url . '/wp-admin/post-new.php?post_type=page',
+  $blog_url . '/wp-admin/post-new.php?post_type=page' => [
     'text' => __('Add new page', 'essentials'),
     'icon' => 'file-text-16.svg',
   ],
-  ['url' => $blog_url . '/wp-admin/post-new.php',
+  $blog_url . '/wp-admin/post-new.php' => [
     'text' => __('Add new post', 'essentials'),
     'icon' => 'helparticle-16.svg',
   ],
-  ['url' => $blog_url . '/wp-admin/site-editor.php?postType=wp_navigation',
+  $blog_url . '/wp-admin/site-editor.php?postType=wp_navigation' => [
     'text' => __('Edit site navigation', 'essentials'),
     'icon' => 'digitalisation-48.svg',
   ],
-  ['url' => $blog_url . '/wp-admin/site-editor.php?path=%2Fwp_global_styles',
+  $blog_url . '/wp-admin/site-editor.php?path=%2Fwp_global_styles' => [
     'text' => __('Change styles, colours and font', 'essentials'),
     'icon' => 'favstar-16.svg',
   ],
-  ['url' => $blog_url . '/wp-admin/site-editor.php',
+  $blog_url . '/wp-admin/site-editor.php' => [
     'text' => __('Edit Header', 'essentials'),
     'icon' => 'website-48.svg',
   ],
-  ['url' => $blog_url . '/wp-admin/site-editor.php',
+  $blog_url . '/wp-admin/site-editor.php' => [
     'text' => __('Edit Footer', 'essentials'),
     'icon' => 'download-document-14.svg',
   ],
-  ['url' => $blog_url . '/wp-admin/plugin-install.php',
+  $blog_url . '/wp-admin/plugin-install.php' => [
     'text' => __('Add plugins', 'essentials'),
     'icon' => 'plus-16.svg',
   ],
-  ['url' => $blog_url . '/wp-admin/upload.php',
+  $blog_url . '/wp-admin/upload.php' => [
     'text' => __('Upload media files', 'essentials'),
     'icon' => 'upload-16.svg',
   ],
