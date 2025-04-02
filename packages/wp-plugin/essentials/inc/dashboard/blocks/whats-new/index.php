5c30115d5bea8ed142c04ca5c3e32906d09881c2
Tobias <tobias.bode@ionos.com>
2025-04-02 12:20:19 +0200

    feat(whats-new): add image and style content

--- packages/wp-plugin/essentials/inc/dashboard/blocks/whats-new/index.php
+++ packages/wp-plugin/essentials/inc/dashboard/blocks/whats-new/index.php
@@ -16,23 +16,34 @@ use const ionos_wordpress\essentials\PLUGIN_DIR;
 function render_callback()
 {
   return '
-<h3 class="wp-block-heading">' . \esc_html__('What’s New') . '</h3>
+ <div class="whats-new">
+<header>
+<img src="' . \esc_url(\plugins_url('', __FILE__) . '/whats-new.png') . '" alt="' . \esc_attr__(
+    'What’s New'
+  ) . '" class="wp-block-image" />
+<h3 class="wp-block-heading">' .
+  \esc_html__('What’s New') .
+'</h3>
+</header>
+<content>
 <p>' . \esc_html__(
-    'Discover new features and get an overview of the latest updates and enhancements of your WordPress Hub'
-  ) . '</p>
-<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--30);margin-bottom:var(--wp--preset--spacing--30);padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:list -->
+  'Discover new features and get an overview of the latest updates and enhancements of your WordPress Hub'
+) .
+'</p>
 <ul class="wp-block-list">
 <li class="has-small-font-size">
-  <h3>' . \esc_html__('Introducing the new WordPress Hub') . '</h3>
-  <p>' . \esc_html__(
-    "We're excited to announce the launch of our new user interface, featuring a completely new design and improved user experience. Managing your WordPress website has never been easier."
-  ) . '</p>
+  <b>' . \esc_html__('Introducing the new WordPress Hub') . '</b>
+  <br>' . \esc_html__(
+  'Featuring a completely new design and an improved user experience, making managing your website easier than ever.'
+) . '
 </li>
 <li class="has-small-font-size">
-  <h3>' . \esc_html__('Vulnerability Scan Results') . '</h3>
-  <p>' . \esc_html__(
-    "We've added vulnerability scan results directly to your dashboard, giving you instant visibility into potential security risks to keep your WordPress site protected."
-  ) . '</p>
+  <b>' . \esc_html__('Vulnerability Scan Results') . '</b>
+  <br>' . \esc_html__(
+  "We've added scan results directly to your dashboard, giving you instant visibility into potential security risks."
+) . '
 </li>
-';
+</ul>
+</content>
+</div>';
 }
