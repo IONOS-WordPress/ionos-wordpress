7d0e93ffd65900163af600e1e1698f6d2cc8c907
Marko Feldmann <46565390+DerHerrFeldmann@users.noreply.github.com>
2025-04-10 11:37:33 +0200

    update background color for notifications and menu-target-arrow

--- packages/wp-plugin/ionos-essentials/inc/dashboard/index.php
+++ packages/wp-plugin/ionos-essentials/inc/dashboard/index.php
@@ -58,9 +58,13 @@ require_once __DIR__ . '/blocks/whats-new/index.php';
         }
         #wpwrap #wpcontent {
           padding-left: 0;
+          background-color: #f4f7fa;
         }
         .wp-responsive-open #wpbody {
           right: -13em !important;
+        }
+        #wpwrap ul#adminmenu a.wp-has-current-submenu:after, #wpwrap ul#adminmenu > li.current > a.current:after {
+          border-right-color: #f4f7fa;
         }
         EOF
     );
