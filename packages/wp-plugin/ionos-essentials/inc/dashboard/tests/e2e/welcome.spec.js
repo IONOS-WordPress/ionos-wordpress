40ff15cc43cafde49897b1c4ef270ec00a014de7
Lars Gersmann <lars.gersmann@ionos.com>
2025-04-28 11:41:40 +0200

    refactor(tests): add tags to e2e tests

--- packages/wp-plugin/ionos-essentials/inc/dashboard/tests/e2e/welcome.spec.js
+++ packages/wp-plugin/ionos-essentials/inc/dashboard/tests/e2e/welcome.spec.js
@@ -1,38 +1,44 @@
 import { test, expect } from '@wordpress/e2e-test-utils-playwright';
 import { execSync } from 'node:child_process';
 
-test.describe('essentials:dashboard next-best-actions block', () => {
-  test.beforeAll(async () => {
-    try {
-      await execSync('pnpm -s wp-env run tests-cli wp rewrite structure /%postname% --hard');
-      await execSync('pnpm -s wp-env run tests-cli wp user meta delete 1 ionos_essentials_welcome');
-    } catch (error) {}
-  });
+test.describe(
+  'essentials:dashboard next-best-actions block',
+  {
+    tag: ['@dashboard', '@welcome', '@editor'],
+  },
+  () => {
+    test.beforeAll(async () => {
+      try {
+        await execSync('pnpm -s wp-env run tests-cli wp rewrite structure /%postname% --hard');
+        await execSync('pnpm -s wp-env run tests-cli wp user meta delete 1 ionos_essentials_welcome');
+      } catch (error) {}
+    });
 
-  test('test welcome banner', async ({ admin, page }) => {
-    // show dashboard and click on dismiss button of "create-page" action
-    await admin.visitAdminPage('/');
-    // get the iframe element
-    let iframeLocator = await page.locator('iframe');
-    // get the iframe's body element
-    let iframeBodyLocator = await iframeLocator.contentFrame().locator('body');
+    test('test welcome banner', async ({ admin, page }) => {
+      // show dashboard and click on dismiss button of "create-page" action
+      await admin.visitAdminPage('/');
+      // get the iframe element
+      let iframeLocator = await page.locator('iframe');
+      // get the iframe's body element
+      let iframeBodyLocator = await iframeLocator.contentFrame().locator('body');
 
-    let dialogBox = await iframeBodyLocator.locator('#essentials-welcome_block');
-    await expect(dialogBox).toHaveCount(1);
-    await expect(dialogBox).toHaveAttribute('open');
+      let dialogBox = await iframeBodyLocator.locator('#essentials-welcome_block');
+      await expect(dialogBox).toHaveCount(1);
+      await expect(dialogBox).toHaveAttribute('open');
 
-    await dialogBox.locator('button').click();
-    await expect(dialogBox).not.toHaveAttribute('open');
-  });
+      await dialogBox.locator('button').click();
+      await expect(dialogBox).not.toHaveAttribute('open');
+    });
 
-  test('test welcome is still closed', async ({ admin, page }) => {
-    // show dashboard and click on dismiss button of "create-page" action
-    await admin.visitAdminPage('/');
+    test('test welcome is still closed', async ({ admin, page }) => {
+      // show dashboard and click on dismiss button of "create-page" action
+      await admin.visitAdminPage('/');
 
-    let iframeLocator = await page.locator('iframe');
-    let iframeBodyLocator = await iframeLocator.contentFrame().locator('body');
+      let iframeLocator = await page.locator('iframe');
+      let iframeBodyLocator = await iframeLocator.contentFrame().locator('body');
 
-    let dialogBox = await iframeBodyLocator.locator('#essentials-welcome_block');
-    await expect(dialogBox).toHaveCount(0);
-  });
-});
+      let dialogBox = await iframeBodyLocator.locator('#essentials-welcome_block');
+      await expect(dialogBox).toHaveCount(0);
+    });
+  }
+);
