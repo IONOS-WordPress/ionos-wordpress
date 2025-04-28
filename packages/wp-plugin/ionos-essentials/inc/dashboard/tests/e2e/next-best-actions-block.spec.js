f89bfd21d4923d52fd4c85860f24286b611ecad0
Tobias <tobias.bode@ionos.com>
2025-04-28 12:01:15 +0200

    remove unnecessary awaits

--- packages/wp-plugin/ionos-essentials/inc/dashboard/tests/e2e/next-best-actions-block.spec.js
+++ packages/wp-plugin/ionos-essentials/inc/dashboard/tests/e2e/next-best-actions-block.spec.js
@@ -8,14 +8,14 @@ test.describe('essentials:dashboard next-best-actions block', () => {
     await execSync('pnpm -s wp-env run tests-cli wp rewrite structure /%postname% --hard');
     await execSync('pnpm -s wp-env run tests-cli wp user meta update 1 ionos_essentials_welcome true');
 
-    // Install nessesary plugins
+    // Install necessary plugins
     await execSync('pnpm -s wp-env run tests-cli wp plugin install --activate extendify --force');
     await execSync(
       'pnpm -s wp-env run tests-cli wp plugin install --activate https://web-hosting.s3-eu-central-1.ionoscloud.com/extendify/01-ext-ion2hs971.zip --force'
     );
-
     // Install the extendable theme
     await execSync('pnpm -s wp-env run tests-cli wp theme install --activate extendable --force');
+    // avoid the redirect leading to the switch page. done for redirect_count > 3
     await execSync('pnpm -s wp-env run tests-cli wp option update extendify_attempted_redirect_count 4');
   });
 
@@ -23,42 +23,41 @@ test.describe('essentials:dashboard next-best-actions block', () => {
     // show dashboard and click on dismiss button of "create-page" action
     await admin.visitAdminPage('/');
     // get the iframe element
-    let iframeLocator = await page.locator('iframe');
+    let iframeLocator = page.locator('iframe');
     // get the iframe's body element
-    let iframeBodyLocator = await iframeLocator.contentFrame().locator('body');
+    let iframeBodyLocator = iframeLocator.contentFrame().locator('body');
 
     // get dismiss anchor element
-    let dismissAncor = await iframeBodyLocator.locator('css=.dismiss-nba[data-nba-id="edit-and-complete"]');
+    let dismissAncor = iframeBodyLocator.locator('css=.dismiss-nba[data-nba-id="edit-and-complete"]');
     await expect(dismissAncor).toHaveCount(1);
     await dismissAncor.click();
 
     // show dashboard and ensure "create-page" action is not more available
     await admin.visitAdminPage('/');
-    iframeLocator = await page.locator('iframe');
-    iframeBodyLocator = await iframeLocator.contentFrame().locator('body');
-    dismissAncor = await iframeBodyLocator.locator('css=.dismiss-nba[data-nba-id="edit-and-complete"]');
+    iframeLocator = page.locator('iframe');
+    iframeBodyLocator = iframeLocator.contentFrame().locator('body');
+    dismissAncor = iframeBodyLocator.locator('css=.dismiss-nba[data-nba-id="edit-and-complete"]');
     await expect(dismissAncor).toHaveCount(0);
   });
 
   test('test help center action behavior', async ({ admin, page }) => {
-    // redirect to admin page and check if the help center cart exists => expectation is true
     await admin.visitAdminPage('/');
-    let iframeLocator = await page.locator('iframe');
+    let iframeLocator = page.locator('iframe');
     let iframeBodyLocator = await iframeLocator.contentFrame().locator('body');
-    let nbaLink = await iframeBodyLocator.locator('css=.nba-link[data-nba-id="help-center"]');
+    let nbaLink = iframeBodyLocator.locator('css=.nba-link[data-nba-id="help-center"]');
     await expect(nbaLink).toHaveCount(1);
 
     // click help center cart action and check if the help center flyIn  is open
-    let headlessui = await page.locator('#headlessui-portal-root');
+    let headlessui = page.locator('#headlessui-portal-root');
     await expect(headlessui).toHaveCount(0);
     await nbaLink.click();
     await expect(headlessui).toHaveCount(1);
 
-    // redirect to admin page and check if the help center cart exists => expectation is false
+    // redirect to admin page and check if the help center card exists => expectation is false
     await admin.visitAdminPage('/');
-    iframeLocator = await page.locator('iframe');
-    iframeBodyLocator = await iframeLocator.contentFrame().locator('body');
-    nbaLink = await iframeBodyLocator.locator('css=.nba-link[data-nba-id="help-center"]');
+    iframeLocator = page.locator('iframe');
+    iframeBodyLocator = iframeLocator.contentFrame().locator('body');
+    nbaLink = iframeBodyLocator.locator('css=.nba-link[data-nba-id="help-center"]');
     await expect(nbaLink).toHaveCount(0);
   });
 
@@ -70,7 +69,6 @@ test.describe('essentials:dashboard next-best-actions block', () => {
     await execSync(
       'pnpm -s wp-env run tests-cli wp media import --skip-copy --title="test-logo" wp-content/plugins/ionos-essentials/inc/dashboard/data/tenant-logos/welcome-banner.png '
     );
-    // redirect to admin page and check if the help center cart exists => expectation is true
     await admin.visitAdminPage('/');
     let iframeLocator = await page.locator('iframe');
     let iframeBodyLocator = await iframeLocator.contentFrame().locator('body');
