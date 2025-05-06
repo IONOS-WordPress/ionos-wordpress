789743f9ab747726051cfff02cc7515c1c36c307
peperuda-dev <denise.stamatov@ionos.com>
2025-05-06 09:18:30 +0200

    Feature/gphwpp 3860 add gml to nba (#300)
    * add nba-action for gml plugin

--- packages/wp-plugin/ionos-essentials/inc/dashboard/blocks/next-best-actions/class-nba.php
+++ packages/wp-plugin/ionos-essentials/inc/dashboard/blocks/next-best-actions/class-nba.php
@@ -177,6 +177,20 @@ if (is_plugin_active('woocommerce/woocommerce.php')) {
   );
 }
 
+$tenant      = strtolower(\get_option('ionos_group_brand', 'ionos'));
+$market        = strtolower(\get_option($tenant . '_market', 'de'));
+
+if ( $market === 'de' && is_plugin_active('woocommerce/woocommerce.php') && ! is_plugin_active('woocommerce-german-market-light/woocommerce-german-market-light.php') ) {
+  NBA::register(
+    id: 'woocommerce-gml',
+    title: \__('Legally compliant selling with German Market Light', 'ionos-essentials'),
+    description: \__('Use the free extension for WooCommerce to operate your online store in Germany and Austria in a legally compliant manner.', 'ionos-essentials'),
+    link: '#',
+    anchor: \__('Install now', 'ionos-essentials'),
+    completed: is_plugin_active('woocommerce-german-market-light/WooCommerce-German-Market-Light.php'), // when gml is installed and activate
+  );
+}
+
 if ('extendable' === get_stylesheet()) {
   $custom_logo_id           = get_theme_mod('custom_logo');
   $logo                     = wp_get_attachment_image_src($custom_logo_id, 'full');
--- packages/wp-plugin/ionos-essentials/inc/dashboard/blocks/next-best-actions/index.php
+++ packages/wp-plugin/ionos-essentials/inc/dashboard/blocks/next-best-actions/index.php
@@ -2,6 +2,12 @@
 
 namespace ionos\essentials\dashboard\blocks\next_best_actions;
 
+require_once ABSPATH . 'wp-admin/includes/plugin.php';
+require_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
+require_once ABSPATH . 'wp-admin/includes/plugin-install.php';
+require_once ABSPATH . 'wp-admin/includes/file.php';
+require_once ABSPATH . 'wp-admin/includes/misc.php';
+
 use const ionos\essentials\PLUGIN_DIR;
 
 \add_action('init', function () {
@@ -53,13 +59,18 @@ function render_callback()
           <h4 class="wp-block-heading">' . \esc_html($action->title) . '</h4>
           <p>' . \esc_html($action->description) . '</p>
           <div class="wp-block-buttons is-layout-flex wp-block-buttons-is-layout-flex">
-              <div class="wp-block-button">
-                  <a data-nba-id="' . $action->id . '" href="' . \esc_url(
+              <div class="wp-block-button">';
+
+              if($action->id === 'woocommerce-gml') {
+                $body .= '<button style="height: auto;" id="ionos_essentials_install_gml" class="wp-block-button__link wp-element-button nba-link">' . $action->anchor . '</button>';
+              } else {
+                $body .= '<a data-nba-id="' . $action->id . '" href="' . \esc_url(
       $action->link
     ) . '" class="wp-block-button__link wp-element-button nba-link" target="' . $target . '">' . \esc_html(
       $action->anchor
-    ) . '</a>
-              </div>
+    ) . '</a>';
+  }
+             $body .= '</div>
               <div class="wp-block-button is-style-outline is-style-outline--1">
                   <a data-nba-id="' . $action->id . '" class="wp-block-button__link wp-element-button dismiss-nba" target="_top">' . \esc_html__(
       'Dismiss',
@@ -70,6 +81,30 @@ function render_callback()
           </div>
       </div>';
   }
+
+  $body .= '<script>document.querySelector("#ionos_essentials_install_gml").addEventListener("click", function(event) {
+    event.target.disabled = true;
+    event.target.innerText = "' . \esc_js(__('Installing...', 'ionos-essentials')) . '";
+
+    fetch("/wp-json/ionos/essentials/dashboard/nba/v1/install-gml", {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        "X-WP-Nonce": "' . \wp_create_nonce('wp_rest') . '"
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.status === "success") {
+        location.reload();
+      } else {
+        console.error("Failed to install the GML plugin.");
+      }
+    })
+    .catch(error => console.error("Error:", error));
+  });
+  </script></script>';
 
   if (empty($body)) {
     return;
@@ -112,7 +147,71 @@ function render_callback()
       return \current_user_can('manage_options');
     },
   ]);
+
+  \register_rest_route('ionos/essentials/dashboard/nba/v1', 'install-gml', [
+      'methods'             => 'GET',
+      'permission_callback' => function () {
+        return \current_user_can('install_plugins');
+      },
+      'callback'            => function () {
+        $plugin_slug = 'woocommerce-german-market-light/WooCommerce-German-Market-Light.php';
+        if (! file_exists(WP_PLUGIN_DIR . '/' . $plugin_slug)) {
+          if (! install_plugin_from_url(
+            'https://marketpress.de/mp-download/no-key-woocommerce-german-market-light/woocommerce-german-market-light/1und1/'
+          )) {
+            return new \WP_REST_Response([
+              'status' => 'error',
+            ], 500);
+          }
+        }
+        \activate_plugin($plugin_slug, '', false, true);
+
+        return new \WP_REST_Response([
+          'status' => 'success',
+        ], 200);
+      },
+    ]
+  );
+
+
 });
+
+
+function install_plugin_from_url($plugin_url)
+{
+  class Silent_Skin extends \WP_Upgrader_Skin
+  {
+    public function feedback($string, ...$args)
+    {
+    }
+
+    public function header()
+    {
+    }
+
+    public function footer()
+    {
+    }
+
+    public function error($errors)
+    {
+    }
+
+    public function before()
+    {
+    }
+
+    public function after()
+    {
+    }
+  }
+
+  $skin     = new Silent_Skin();
+  $upgrader = new \Plugin_Upgrader($skin);
+  $result   = $upgrader->install($plugin_url);
+
+  return ! is_wp_error($result);
+}
 
 \add_action('post_updated', function ($post_id, $post_after, $post_before) {
   if ('publish' !== $post_before->post_status || ('publish' !== $post_after->post_status && 'draft' !== $post_after->post_status)) {
