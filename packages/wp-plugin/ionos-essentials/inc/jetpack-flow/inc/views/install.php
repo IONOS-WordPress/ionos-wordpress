11a2594d50f3da79dde573c75e1b4907f6d32fc8
Denise Stamatov <denise.stamatov@ionos.com>
2025-05-26 15:36:48 +0200

    Update install.php

--- packages/wp-plugin/ionos-essentials/inc/jetpack-flow/inc/views/install.php
+++ packages/wp-plugin/ionos-essentials/inc/jetpack-flow/inc/views/install.php
@@ -1,10 +1,7 @@
 <div class="wrapper">
-  <div class="header">
-      <img class="logo" src="<?php echo ( 'branding.logo_variant1' ); ?>" />
-  </div>
   <div class="container">
     <h1 class="screen-reader-text"><?php _e( 'Installing Jetpack Backup', 'ionos-assistant' ); ?></h1>
-    <img src="<?php echo plugins_url( '/img/jetpack-logo.svg' ); ?>" alt="" class="jetpack-logo">
+    <img src="<?php echo esc_url(\plugins_url('', __DIR__).  '/img/jetpack-logo.svg'); ?>" class="jetpack-logo" alt="">
     <p><?php _e( 'Please wait a moment while we are installing Jetpack Backup for you.', 'ionos-assistant' ); ?></p>
   </div>
 </div>
