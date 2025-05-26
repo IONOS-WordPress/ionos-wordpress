2cc850a65278b628a6d9300624a4b8db235f0102
Denise Stamatov <denise.stamatov@ionos.com>
2025-05-26 15:35:59 +0200

    update img url

--- packages/wp-plugin/ionos-essentials/inc/jetpack-flow/inc/views/confirm.php
+++ packages/wp-plugin/ionos-essentials/inc/jetpack-flow/inc/views/confirm.php
@@ -3,9 +3,6 @@ use ionos\essentials\jetpack_flow\Manager;
 ?>
 
 <div class="wrapper">
-  <div class="header">
-      <img class="logo" src="<?php echo( 'branding.logo_variant1' ); ?>" />
-  </div>
   <div class="container">
     <form>
       <input type="hidden" name="coupon" value="<?php echo $_GET['coupon']; ?>">
@@ -13,7 +10,7 @@ use ionos\essentials\jetpack_flow\Manager;
       <input type="hidden" name="step" value="install">
 
       <h1 class="screen-reader-text"><?php _e( 'Jetpack Backup', 'ionos-assistant' ); ?></h1>
-      <img src="<?php echo plugins_url( '/img/jetpack-logo.svg'); ?>" class="jetpack-logo" alt="">
+      <img src="<?php echo esc_url(\plugins_url('', __DIR__).  '/img/jetpack-logo.svg'); ?>" class="jetpack-logo" alt="">
       <p><?php _e( 'We are going to install Jetpack Backup now.', 'ionos-assistant' ); ?></p>
       <div class="buttons">
         <button class="btn primarybtn" type="submit"><?php _e( 'Ok', 'ionos-assistant' ); ?></button>
