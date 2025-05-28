c1870b132935f3436c22e98c4811e746e0c82e00
Lars Gersmann <lars.gersmann@ionos.com>
2025-05-28 10:40:10 +0200

    fix: use esc_attr_e and esc_html_e for improved security in install.php

--- packages/wp-plugin/ionos-essentials/inc/jetpack-flow/inc/views/install.php
+++ packages/wp-plugin/ionos-essentials/inc/jetpack-flow/inc/views/install.php
@@ -1,7 +1,7 @@
 <div class="wrapper">
   <div class="container">
     <h1 class="screen-reader-text"><?php __('Installing Jetpack Backup', 'ionos-essentials'); ?></h1>
-    <img src="<?php echo esc_attr(\plugins_url('', __DIR__) . '/img/jetpack-logo.svg'); ?>" class="jetpack-logo" alt="">
-    <p><?php __('Please wait a moment while we are installing Jetpack Backup for you.', 'ionos-essentials'); ?></p>
+    <img src="<?php esc_attr_e(\plugins_url('', __DIR__) . '/img/jetpack-logo.svg'); ?>" class="jetpack-logo" alt="">
+    <p><?php esc_html_e('Please wait a moment while we are installing Jetpack Backup for you.', 'ionos-essentials'); ?></p>
   </div>
 </div>
