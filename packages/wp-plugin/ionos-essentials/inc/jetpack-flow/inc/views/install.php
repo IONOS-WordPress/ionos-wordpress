3d3f8a31ac4c6dd07a318300605e3ed31b4fe372
Lars Gersmann <lars.gersmann@ionos.com>
2025-05-28 10:27:21 +0200

    fix: replace esc_url with esc_attr for image source in install.php

--- packages/wp-plugin/ionos-essentials/inc/jetpack-flow/inc/views/install.php
+++ packages/wp-plugin/ionos-essentials/inc/jetpack-flow/inc/views/install.php
@@ -1,7 +1,7 @@
 <div class="wrapper">
   <div class="container">
     <h1 class="screen-reader-text"><?php __('Installing Jetpack Backup', 'ionos-essentials'); ?></h1>
-    <img src="<?php echo esc_url(\plugins_url('', __DIR__) . '/img/jetpack-logo.svg'); ?>" class="jetpack-logo" alt="">
+    <img src="<?php echo esc_attr(\plugins_url('', __DIR__) . '/img/jetpack-logo.svg'); ?>" class="jetpack-logo" alt="">
     <p><?php __('Please wait a moment while we are installing Jetpack Backup for you.', 'ionos-essentials'); ?></p>
   </div>
 </div>
