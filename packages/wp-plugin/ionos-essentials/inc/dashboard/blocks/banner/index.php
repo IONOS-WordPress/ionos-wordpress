c246cd9db4777deaee484d6cf41a1fdc6c2fac52
Thomas Rose <thomas.rose@ionos.com>
2025-06-24 09:17:32 +0200

    introduce tabs

--- packages/wp-plugin/ionos-essentials/inc/dashboard/blocks/banner/index.php
+++ packages/wp-plugin/ionos-essentials/inc/dashboard/blocks/banner/index.php
@@ -38,19 +38,13 @@ function render_callback(): void
     <div class="card__content">
       <section class="card__section">
         <div class="grid grid--vertical-align-center">
-          <div class="grid-col grid-col--4 grid-col--small-12">
+          <div class="grid-col grid-col--6 grid-col--small-12">
             <img class=""
             src="<?php echo esc_attr($tenant_logo); ?>"
             alt="<?php echo \esc_attr($tenant_name); ?> Logo"
             style="width: 150px; padding: 50px;"
           >
           </div>
-
-          <div class="grid-col grid-col--2 grid-col--small-12 ionos_banner_buttons">
-            <button class="ionos-tab-button ghost-button" data-tab="ionos-tab1"><?php esc_html_e('Overview', 'ionos-essentials'); ?></button>
-            <button class="ionos-tab-button ghost-button" data-tab="ionos-tab2"><?php esc_html_e('Security', 'ionos-essentials'); ?></button>
-          </div>
-
           <div class="grid-col grid-col--6 grid-col--small-12 ionos_banner_buttons">
             <?php echo \wp_kses($buttons, 'post'); ?>
           </div>
