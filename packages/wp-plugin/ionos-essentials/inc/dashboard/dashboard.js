d897a57f0998795777a6a55c27f4225e29cabb80
Thomas Rose <thomas.rose@ionos.com>
2025-06-24 09:25:48 +0200

    introduce tabs

--- packages/wp-plugin/ionos-essentials/inc/dashboard/dashboard.js
+++ packages/wp-plugin/ionos-essentials/inc/dashboard/dashboard.js
@@ -1,20 +1,45 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function () {
   // Welcome dialog
-  const dashboard = document.querySelector("#wpbody-content").shadowRoot
-  const dialog = dashboard.querySelector("#essentials-welcome_block");
-  const closeButton = dashboard.querySelector(".button--primary");
+  const dashboard = document.querySelector('#wpbody-content').shadowRoot;
+  const dialog = dashboard.querySelector('#essentials-welcome_block');
+  const closeButton = dashboard.querySelector('.button--primary');
 
-  closeButton.addEventListener("click", function() {
-      dialog.close();
-      fetch( wpData.restUrl + 'ionos/essentials/dashboard/welcome/v1/closer', {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          "X-WP-Nonce": wpData.nonce
-        },
-        credentials: "include",
-      })
+  closeButton.addEventListener('click', function () {
+    dialog.close();
+    fetch(wpData.restUrl + 'ionos/essentials/dashboard/welcome/v1/closer', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-WP-Nonce': wpData.nonce,
+      },
+      credentials: 'include',
+    });
   });
+
+  // Tabs
+  const tabButtons = dashboard.querySelectorAll('[data-tab]');
+  tabButtons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      const activeTabs = dashboard.querySelectorAll('.ionos-tab.active, [data-tab].active');
+      activeTabs.forEach((tab) => {
+        tab.classList.remove('active');
+      });
+
+      event.target.classList.add('active');
+
+      const targetTab = event.target.getAttribute('data-tab');
+      dashboard.querySelector(`#${targetTab}`).classList.add('active');
+      window.location.hash = targetTab;
+    });
+  });
+  const anchorId = window.location.hash.substring(1);
+  if (anchorId) {
+    dashboard.querySelector(`[data-tab="${anchorId}"]`)?.click();
+  } else {
+    dashboard.querySelector('[data-tab]')?.click();
+  }
 
   // NBA
   dashboard.querySelectorAll('.ionos-dismiss-nba').forEach((el) => {
@@ -24,35 +49,35 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 
-    const emailAccountLink = dashboard.querySelector('a[data-nba-id="email-account"]');
-    if (emailAccountLink) {
-      emailAccountLink.onclick = () => {
-        dismissItem(emailAccountLink);
-      };
-    }
+  const emailAccountLink = dashboard.querySelector('a[data-nba-id="email-account"]');
+  if (emailAccountLink) {
+    emailAccountLink.onclick = () => {
+      dismissItem(emailAccountLink);
+    };
+  }
 
-    const helpCenterLink = document.querySelector('a[data-nba-id="help-center"]');
-    if (helpCenterLink) {
-      helpCenterLink.onclick = () => {
-        document.querySelector('.extendify-help-center button').click();
-        dismissItem(helpCenterLink);
-      };
-    }
+  const helpCenterLink = document.querySelector('a[data-nba-id="help-center"]');
+  if (helpCenterLink) {
+    helpCenterLink.onclick = () => {
+      document.querySelector('.extendify-help-center button').click();
+      dismissItem(helpCenterLink);
+    };
+  }
 
   const dismissItem = async (target) => {
-    fetch( wpData.restUrl + 'ionos/essentials/dashboard/nba/v1/dismiss/' + target.dataset.nbaId, {
-      method: "POST",
+    fetch(wpData.restUrl + 'ionos/essentials/dashboard/nba/v1/dismiss/' + target.dataset.nbaId, {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
-        "X-WP-Nonce": wpData.nonce
+        'Content-Type': 'application/json',
+        'X-WP-Nonce': wpData.nonce,
       },
-      credentials: "include",
-   }).then(response => {
+      credentials: 'include',
+    }).then((response) => {
       if (!response.ok) {
         return;
       }
 
-       dashboard.getElementById(target.dataset.nbaId).classList.add('ionos_nba_dismissed');
+      dashboard.getElementById(target.dataset.nbaId).classList.add('ionos_nba_dismissed');
       setTimeout(() => {
         dashboard.getElementById(target.dataset.nbaId).remove();
 
@@ -60,29 +85,27 @@ document.addEventListener("DOMContentLoaded", function() {
         if (nbaCount === 0) {
           dashboard.querySelector('.ionos_next_best_actions').remove();
         }
-    }, 800);
+      }, 800);
     });
   };
 
-  dashboard.querySelector("#ionos_essentials_install_gml")?.addEventListener("click", function(event) {
-
+  dashboard.querySelector('#ionos_essentials_install_gml')?.addEventListener('click', function (event) {
     event.target.disabled = true;
-    event.target.innerText =  wpData.i18n.installing;
+    event.target.innerText = wpData.i18n.installing;
 
-    fetch(wpData.restUrl +  'ionos/essentials/dashboard/nba/v1/install-gml', {
-      method: "GET",
-      credentials: "include",
+    fetch(wpData.restUrl + 'ionos/essentials/dashboard/nba/v1/install-gml', {
+      method: 'GET',
+      credentials: 'include',
       headers: {
-        "Content-Type": "application/json",
-        "X-WP-Nonce": wpData.nonce
-      }
+        'Content-Type': 'application/json',
+        'X-WP-Nonce': wpData.nonce,
+      },
     })
-    .then(response => response.json())
-    .then(data => {
-      if (data.status === "success") {
-        location.reload();
-      }
-    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status === 'success') {
+          location.reload();
+        }
+      });
   });
-
 });
--- packages/wp-plugin/ionos-essentials/inc/dashboard/view.php
+++ packages/wp-plugin/ionos-essentials/inc/dashboard/view.php
@@ -63,22 +63,28 @@ use const ionos\essentials\PLUGIN_DIR;
 
 <main id="content">
   <div class="page-section">
-  <?php blocks\banner\render_callback(); ?>
+    <?php blocks\banner\render_callback(); ?>
   </div>
 
-<div class="page-section">
-  <div class="grid grid--full-height">
-    <!--
-      There must be no whitespace or newline within the .grid-col elements, as this would display even an empty cell,
-      but empty cells should not be displayed (see)
-    -->
-    <div class="grid-col grid-col--12 ionos_next_best_actions"><?php blocks\next_best_actions\render_callback(); ?></div>
-    <div class="grid-col grid-col--4 grid-col--small-12"><?php blocks\vulnerability\render_callback(); ?></div>
-    <div class="grid-col grid-col--8 grid-col--small-12"><?php blocks\quick_links\render_callback(); ?></div>
-    <div class="grid-col grid-col--5 grid-col--medium-6 grid-col--small-12"><?php blocks\my_account\render_callback(); ?></div>
-    <div class="grid-col grid-col--7 grid-col--medium-6 grid-col--small-12"><?php blocks\whatsnew\render_callback(); ?></div>
+  <div id="ionos-tab1" class="page-section ionos-tab">
+    <div class="grid grid--full-height">
+      <!--
+        There must be no whitespace or newline within the .grid-col elements, as this would display even an empty cell,
+        but empty cells should not be displayed (see)
+      -->
+      <div class="grid-col grid-col--12 ionos_next_best_actions"><?php blocks\next_best_actions\render_callback(); ?></div>
+      <div class="grid-col grid-col--4 grid-col--small-12"><?php blocks\vulnerability\render_callback(); ?></div>
+      <div class="grid-col grid-col--8 grid-col--small-12"><?php blocks\quick_links\render_callback(); ?></div>
+      <div class="grid-col grid-col--5 grid-col--medium-6 grid-col--small-12"><?php blocks\my_account\render_callback(); ?></div>
+      <div class="grid-col grid-col--7 grid-col--medium-6 grid-col--small-12"><?php blocks\whatsnew\render_callback(); ?></div>
+    </div>
   </div>
-</div>
+
+  <div id="ionos-tab2" class="page-section ionos-tab">
+    <div class="grid grid--full-height">
+      <div class="grid-col grid-col--7 grid-col--medium-12 grid-col--small-12"><?php blocks\vulnerability\render_callback(); ?></div>
+    </div>
+  </div>
 
 </main>
 </template>
