2bcf634814adb83e8fc118add64e1f92eeedc918
Tobias Bode <tobias.bode@ionos.com>
2025-07-03 14:15:23 +0200

    demo

--- packages/docker/rector-php/rector-config-php7.4.php
+++ packages/docker/rector-php/rector-config-php7.4.php
@@ -11,26 +11,4 @@ return RectorConfig::configure()->withSkip([__DIR__ . '/dist/vendor', __DIR__ . 
   // see https://github.com/rectorphp/rector-src/blob/3ed476b9ab65958d85416e48a810b11dbaf4283a/build/config/config-downgrade.php
   //->withPHPStanConfigs([__DIR__ . '/phpstan-for-downgrade.neon'])
   ->withPhpVersion(PhpVersion::PHP_83)
-  /*
-  ->withPreparedSets(
-    // deadCode: true,
-    // codeQuality: true,
-    // codingStyle: true,
-    // typeDeclarations: true,
-    // privatization: true,
-    // naming: true,
-    // instanceOf: true,
-    // earlyReturn: true,
-    // strictBooleans: true,
-    // carbon: true,
-    // rectorPreset: true,
-    // phpunitCodeQuality: true,
-    // doctrineCodeQuality: true,
-    // symfonyCodeQuality: true,
-    // symfonyConfigs: true,
-    // // composer based
-    // twig: true,
-    // phpunit: true,
-  )
-  */
   ->withDowngradeSets(php74: true);
