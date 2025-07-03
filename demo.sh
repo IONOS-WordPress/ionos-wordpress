172fe1ee47bc4359ae880afbe566e77feb6f7f3d
Tobias Bode <tobias.bode@ionos.com>
2025-07-03 12:40:07 +0200

    demo

--- demo.sh
+++ demo.sh
@@ -1,6 +1,3 @@
-docker run   --rm   --user "1000:1000"   -v $(pwd)/packages/wp-plugin/ionos-essentials:/project/dist   -v $(pwd)/packages/docker/rector-php/rector-code-quality.php:/project/rector-code-quality.php   ionos-wordpress/rector-php   --clear-cache   --config "rector-code-quality.php" --debug  process dist
-
-
 docker run \
   --rm \
   --user "1000:1000" \
