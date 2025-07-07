d077e318c7e38ff1b6370cc6c424c00d98d27dfd
Denise Stamatov <denise.stamatov@ionos.com>
2025-07-07 17:18:04 +0200

    Update pel.php

--- packages/wp-plugin/ionos-essentials/inc/security/pel.php
+++ packages/wp-plugin/ionos-essentials/inc/security/pel.php
@@ -2,30 +2,26 @@
 
 namespace ionos\essentials\security;
 
+use const ionos\essentials\security\IONOS_SECURITY_FEATURE_OPTION;
+use const ionos\essentials\security\IONOS_SECURITY_FEATURE_OPTION_PEL;
+
 // exit if accessed directly
 if (! defined('ABSPATH')) {
   exit();
 }
 
-$options = \get_option('IONOS_SECURITY_FEATURE_OPTION', false);
-$options['IONOS_SECURITY_FEATURE_OPTION_PEL'] = false;
-\update_option('IONOS_SECURITY_FEATURE_OPTION', $options);
+add_action( 'login_init' , function () {
+  remove_filter( 'authenticate', 'wp_authenticate_email_password', 20 );
+  add_filter( 'authenticate', __NAMESPACE__ . '\email_auth_filter' , 200, 2 );
+} );
 
-add_action( 'login_init', function () {
-  if ( ! \get_option( 'IONOS_SECURITY_FEATURE_OPTION[IONOS_SECURITY_FEATURE_OPTION_PEL]', false ) ) {
-    return;
-  }
-});
-
-add_action('implement_security_feature_page', function () {
-
-  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $value = true;
-    if ( isset($_POST['ionos_security_pel_enabled']) ) {
-      $value = false;
-    }
-
-    \update_option('IONOS_SECURITY_FEATURE_OPTION[IONOS_SECURITY_FEATURE_OPTION_PEL]', $value);
+function email_auth_filter( $user, $username ) {
+  if ( false !== strpos( $username, '@' ) ) {
+    return new \WP_Error(
+      'email_login_inactive',
+      __( '<strong>Error</strong>: The login with an email address is deactivated for this website. Please use your username instead.', 'ionos-security' )
+    );
   }
 
-});
+  return $user;
+}
