0914409fbb24a036779086187ccd79d9dd343799
Lars Gersmann <lars.gersmann@ionos.com>
2025-07-09 14:41:55 +0200

    squeezed out the real logic of jetpack flow into a single php file

--- packages/wp-plugin/ionos-essentials/inc/jetpack-flow/inc/views/install.php
+++
@@ -1,7 +0,0 @@
-<div class="wrapper">
-  <div class="container">
-    <h1 class="screen-reader-text"><?php esc_html_e('Installing Jetpack Backup', 'ionos-essentials'); ?></h1>
-    <img src="<?php echo esc_attr(\plugins_url('jetpack-logo.svg', __FILE__)); ?>" class="jetpack-logo">
-    <p><?php esc_html_e('Please wait a moment while we are installing Jetpack Backup for you.', 'ionos-essentials'); ?></p>
-  </div>
-</div>
