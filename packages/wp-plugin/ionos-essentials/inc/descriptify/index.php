72be53cef49c177a818ae924c7ea0e9524c7fa37
Lars Gersmann <lars.gersmann@ionos.com>
2025-07-15 13:18:55 +0200

    fix  declaring $control_panel_links  as a global variable

--- packages/wp-plugin/ionos-essentials/inc/descriptify/index.php
+++ packages/wp-plugin/ionos-essentials/inc/descriptify/index.php
@@ -18,15 +18,26 @@ add_action(
       return;
     }
 
-    $config_file = __DIR__ . '/config.php';
-
-    if (! file_exists($config_file)) {
-      return;
-    }
-
-    require $config_file;
     $tenant  = strToLower(\get_option('ionos_group_brand', 'ionos'));
     $market  = strtolower(\get_option($tenant . '_market', ''));
+
+    $control_panel_links = [
+      'ionos'  => [
+        'ca' => 'https://my.ionos.ca/projects-overview',
+        'de' => 'https://mein.ionos.de/projects-overview',
+        'fr' => 'https://my.ionos.fr/projects-overview',
+        'gb' => 'https://my.ionos.co.uk/projects-overview',
+        'it' => 'https://my.ionos.it/projects-overview',
+        'mx' => 'https://my.ionos.mx/projects-overview',
+        'uk' => 'https://my.ionos.co.uk/projects-overview',
+        'us' => 'https://my.ionos.com/projects-overview',
+      ],
+      'piensa' => [
+        'es' => 'https://www.piensasolutions.com/clientes',
+        'us' => 'https://www.piensasolutions.com/clientes',
+        'de' => 'https://www.piensasolutions.com/clientes',
+      ],
+    ];
 
     if (! isset($control_panel_links[$tenant][$market])) {
       return;
