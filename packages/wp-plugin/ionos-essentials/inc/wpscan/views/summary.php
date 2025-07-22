5ba548b0a488cfb1ee3a9847d4f7c8c9552e7deb
Thomas Rose <thomas.rose@ionos.com>
2025-07-22 14:55:16 +0200

    adjust color

--- packages/wp-plugin/ionos-essentials/inc/wpscan/views/summary.php
+++ packages/wp-plugin/ionos-essentials/inc/wpscan/views/summary.php
@@ -20,8 +20,9 @@ function summary()
 
       <div style="display: flex; align-items: center;">
 
-        <div class="paragraph--critical with-issues-only" style="margin-right: 1em;">
-          <i class="exos-icon exos-icon-warningmessage-32"></i>
+        <div class="paragraph--critical" style="margin-right: 1em;">
+          <i class="exos-icon exos-icon-warningmessage-32 with-issues-only"></i>
+          <i class="exos-icon exos-icon-firmationmessage-32 without-issues-only color--success" ></i>
         </div>
         <div>
           <h2 class="headline headline--sub"><?php echo \esc_html__('Vulnerability scan', 'ionos-essentials'); ?></h2>
@@ -38,10 +39,10 @@ function summary()
           </div>
 
           <p class="paragraph without-issues-only">
-            Nothing found, all right.
+            <?php esc_html_e('Website is safe and secure', 'ionos-essentials'); ?>
           </p>
 
-          <p class="paragraph paragraph--small">
+          <p class="paragraph paragraph--neutral">
             <?php
     echo \esc_html(sprintf('Last scan ran %s ago', $wpscan->get_lastscan()));
   ?>
