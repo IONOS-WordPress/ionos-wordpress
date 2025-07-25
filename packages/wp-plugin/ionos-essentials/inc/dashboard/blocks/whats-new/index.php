e5cb9c10ab4420e92e6f253d8cb9ab376f6afe15
Tobias <tobias.bode@ionos.com>
2025-07-25 14:22:04 +0200

    feat(dashboard): enhance 'What's New' block with user feedback survey

--- packages/wp-plugin/ionos-essentials/inc/dashboard/blocks/whats-new/index.php
+++ packages/wp-plugin/ionos-essentials/inc/dashboard/blocks/whats-new/index.php
@@ -15,6 +15,41 @@ use const ionos\essentials\PLUGIN_DIR;
 
 function render_callback()
 {
+  if (strtolower(\get_option('ionos_group_brand', 'ionos')) === 'ionos') {
+  return '
+ <div class="whats-new">
+<header>
+<img src="' . \esc_url(\plugins_url('', __FILE__) . '/whats-new.png') . '" alt="' . \esc_attr__(
+    'What’s New'
+  ) . '" class="wp-block-image" />
+<h3 class="wp-block-heading">' .
+  \esc_html__('What’s New', 'ionos-essentials') .
+'</h3>
+</header>
+<content>
+<p>' . \esc_html__(
+  'Get an overview of the latest updates and enhancements.',
+  'ionos-essentials'
+) .
+'</p>
+<ul class="wp-block-list">
+<li class="has-small-font-size">
+  <b>'. \esc_html__('Your Feedback is important to us', 'ionos-essentials') . '</b>
+  <p>' . \esc_html__(
+    'We\'re always looking for ways to make your WordPress hosting experience even better. Please take a few minutes to fill out a quick online survey.',
+    'ionos-essentials'
+  ) . '</p>
+
+</li>
+<div class="wp-block-button">
+  <button id="ionos_essentials_install_security" class="wp-block-button__link wp-element-button">
+  '. \esc_html__('Take the survey', 'ionos-essentials') .
+  '</button>
+  </div>
+</ul>
+</content>
+</div>';}
+
   return '
  <div class="whats-new">
 <header>
