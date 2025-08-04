ca0db6047af2241ae6f327abe80df56d7931442a
Lars Gersmann <lars.gersmann@ionos.com>
2025-08-04 15:09:08 +0200

    refactor: improved login tentantification

--- packages/wp-plugin/ionos-essentials/inc/login/index.php
+++ packages/wp-plugin/ionos-essentials/inc/login/index.php
@@ -2,63 +2,59 @@
 
 namespace ionos\essentials\login;
 
+use const ionos\essentials\PLUGIN_FILE;
+
 // exit if accessed directly
 if (! defined('ABSPATH')) {
   exit();
 }
 
-add_action('init', function () {
+\add_action('init', function () {
   if (false === in_array($GLOBALS['pagenow'], ['wp-login.php', 'wp-register.php'], true)) {
     return;
   }
 
-  add_action(
+  \add_action(
     'login_enqueue_scripts',
     function () {
-      wp_enqueue_style(
+      \wp_enqueue_style(
         'ionos-login-redesign',
-        plugins_url('style.css', __FILE__),
+        \plugins_url('style.css', __FILE__),
         [],
         filemtime(__DIR__ . '/style.css')
       );
     }
   );
 
-  add_filter('login_body_class', function ($classes) {
+  \add_filter('login_body_class', function ($classes) {
     $classes[] = 'ionos-group-page';
 
     return $classes;
   });
 
-  add_action(
+  \add_action(
     'login_header',
     function () {
       global $interim_login;
-      $template = __DIR__ . '/template.php';
-      if ($interim_login || ! file_exists($template)) {
+      if ($interim_login) {
         return;
       }
 
-      load_template($template);
+      printf( <<<EOF
+        <section class="header">
+          <img
+            src="%s"
+            alt="%s"
+            class="logo"
+          >
+        </section>
+EOF,
+        \esc_attr(\plugins_url(
+        'inc/dashboard/data/tenant-logos/' . \get_option('ionos_group_brand', 'ionos') . '.svg',
+        PLUGIN_FILE
+        )),
+        \esc_attr(\get_option('ionos_group_brand_menu', 'IONOS'))
+      );
     }
   );
 });
-
-function get_brand_config(): array|null
-{
-  static $config = null;
-
-  if (null !== $config) {
-    return $config;
-  }
-
-  $brand_config = __DIR__ . '/configs/' . get_option('ionos_group_brand', 'ionos') . '.php';
-  if (! file_exists($brand_config)) {
-    return null;
-  }
-
-  require_once $brand_config;
-
-  $config = get_config_data();
-  return $config;
-}
