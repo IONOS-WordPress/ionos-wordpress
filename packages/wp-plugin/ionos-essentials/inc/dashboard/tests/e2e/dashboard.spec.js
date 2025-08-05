49721c8db68d91d50fab4b94f8f4f7085c18e5ac
Thomas Rose <thomas.rose@ionos.com>
2025-08-05 11:46:10 +0200

    delete test

--- packages/wp-plugin/ionos-essentials/inc/dashboard/tests/e2e/dashboard.spec.js
+++ packages/wp-plugin/ionos-essentials/inc/dashboard/tests/e2e/dashboard.spec.js
@@ -28,15 +28,5 @@ test.describe(
       await expect(body).toHaveText(/My Account/);
     });
 
-    test.skip('/dashboard visual regression', async ({ admin, page }) => {
-      await page.setViewportSize({ width: 1280, height: 900 });
-      await admin.visitAdminPage('/');
-      const screenshot = await page.screenshot({ fullPage: true });
-      expect(screenshot).toMatchSnapshot('dashboard-overview.png');
-
-      await page.getByRole('button', { name: 'Tools' }).click();
-      const toolsScreenshot = await page.screenshot({ fullPage: true });
-      expect(toolsScreenshot).toMatchSnapshot('dashboard-tools-tab.png');
-    });
   }
 );
