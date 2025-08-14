fce0b7544fcbea7e3c043a6b6ff565adb1465efa
Denise Stamatov <denise.stamatov@ionos.com>
2025-08-14 11:46:04 +0200

    refactor nba block card

--- packages/wp-plugin/ionos-essentials/inc/dashboard/tabs/overview.php
+++ packages/wp-plugin/ionos-essentials/inc/dashboard/tabs/overview.php
@@ -11,7 +11,7 @@ defined('ABSPATH') || exit();
       There must be no whitespace or newline within the .grid-col elements, as this would display even an empty cell,
       but empty cells should not be displayed (see)
     -->
-    <div class="card grid-col grid-col--12 ionos_next_best_actions"><?php blocks\next_best_actions\render_callback(); ?></div>
+    <div class="grid-col grid-col--12"><?php blocks\next_best_actions\render_callback(); ?></div>
     <div class="grid-col grid-col--4 grid-col--small-12"><?php blocks\vulnerability\render_callback(); ?></div>
     <div class="grid-col grid-col--8 grid-col--small-12"><?php blocks\quick_links\render_callback(); ?></div>
     <div class="grid-col grid-col--7 grid-col--medium-6 grid-col--small-12"><?php blocks\my_account\render_callback(); ?></div>
