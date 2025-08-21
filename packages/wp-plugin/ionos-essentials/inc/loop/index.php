4946aa8375f822d59e15a24083f57638d4cc7118
Denise Stamatov <denise.stamatov@ionos.com>
2025-08-21 11:22:18 +0200

    move code

--- packages/wp-plugin/ionos-essentials/inc/loop/index.php
+++ packages/wp-plugin/ionos-essentials/inc/loop/index.php
@@ -9,21 +9,16 @@ defined('ABSPATH') || exit();
 require_once __DIR__ . '/class-plugin.php';
 
 /**
- * Inits the main plugin class.
- */
-\add_action( 'init', function () {
-  Plugin::init();
-} );
-
-
-/**
  * Add consent action on activation.
  */
 \add_action('init', function () {
   if ( get_option( 'ionos_loop_consent' ) === false ) {
     add_option( 'ionos_loop_consent', true );
   }
-	\add_action( 'ionos_loop_consent_given', [ Plugin::class, 'register_at_data_collector' ] );
+
+  Plugin::init();
+
+  \add_action( 'ionos_loop_consent_given', [ Plugin::class, 'register_at_data_collector' ] );
 });
 
 // register_activation_hook( __FILE__, '\ionos\essentials\loop\ionos_add_consent_action' );
