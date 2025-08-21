d97a7e673a4d3125c56b9695a8c056c9c12cc152
Denise Stamatov <denise.stamatov@ionos.com>
2025-08-21 11:19:49 +0200

    add consent option

--- packages/wp-plugin/ionos-essentials/inc/loop/index.php
+++ packages/wp-plugin/ionos-essentials/inc/loop/index.php
@@ -16,17 +16,15 @@ require_once __DIR__ . '/class-plugin.php';
 } );
 
 
-
 /**
  * Add consent action on activation.
  */
-function ionos_add_consent_action() {
-
+\add_action('init', function () {
   if ( get_option( 'ionos_loop_consent' ) === false ) {
     add_option( 'ionos_loop_consent', true );
   }
 	\add_action( 'ionos_loop_consent_given', [ Plugin::class, 'register_at_data_collector' ] );
-}
+});
 
-register_activation_hook( __FILE__, '\ionos\essentials\loop\ionos_add_consent_action' );
+// register_activation_hook( __FILE__, '\ionos\essentials\loop\ionos_add_consent_action' );
 
