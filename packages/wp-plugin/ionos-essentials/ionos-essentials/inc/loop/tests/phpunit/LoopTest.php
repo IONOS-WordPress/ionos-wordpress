2170b7974de089a15951c47a612a94c92809c7f8
Lars Gersmann <lars.gersmann@ionos.com>
2025-08-26 12:00:53 +0200

    refactor(essentials/loop) : added phpunit testcase using the loop endpoint and validating the returned data against the loop json schema

--- packages/wp-plugin/ionos-essentials/ionos-essentials/inc/loop/tests/phpunit/LoopTest.php
+++ packages/wp-plugin/ionos-essentials/ionos-essentials/inc/loop/tests/phpunit/LoopTest.php
@@ -2,8 +2,8 @@
 
 use ionos\essentials\loop;
 
-use const ionos\essentials\migration\WP_OPTION_LAST_INSTALL_DATA;
-use const ionos\essentials\migration\WP_OPTION_LAST_INSTALL_DATA_KEY_PLUGIN_VERSION;
+use const ionos\essentials\loop\IONOS_LOOP_REST_ENDPOINT;
+use const ionos\essentials\loop\IONOS_LOOP_REST_NAMESPACE;
 
 /**
  * run only this test using 'pnpm test:php --php-opts "--filter LoopTest"'
@@ -11,7 +11,9 @@ use const ionos\essentials\migration\WP_OPTION_LAST_INSTALL_DATA_KEY_PLUGIN_VERS
  * @group loop
  * @group essentials
  */
-class LoopTest extends \WP_UnitTestCase {
+class LoopTest extends \WP_UnitTestCase  {
+  private int $admin_user_id;
+  private WP_REST_Server $server;
 
   public function setUp(): void {
     // ensure that post types and taxonomies are reset for each test.
@@ -21,14 +23,28 @@ class LoopTest extends \WP_UnitTestCase {
 
     parent::setUp();
 
+    $this->admin_user_id = \WP_UnitTestCase_Base::factory()->user->create(['role' => 'administrator', 'user_login' => 'test-admin']);
+
     \activate_plugin('ionos-essentials/ionos-essentials.php');
+
+    // Initiating the REST API.
+    global $wp_rest_server;
+    $this->server = $wp_rest_server = new \WP_REST_Server;
+    \do_action( 'rest_api_init' );
   }
 
-  public function test_register_at_datacollector() : void {
-    $this->markTestSkipped('to be implemented');
-  }
+  public function test_rest_loop_endpoint() : void {
+    \wp_set_current_user($this->admin_user_id);
 
-  public function test_rest_loop_data() : void {
-    $this->markTestSkipped('to be implemented');
+    // try to request the loop datacollector endpoint
+    $response = $this->server->dispatch(new \WP_REST_Request('GET', '/' . IONOS_LOOP_REST_NAMESPACE . IONOS_LOOP_REST_ENDPOINT));
+    $this->assertEquals(200, $response->status, 'loop endpoint returned 200');
+
+    $json = $response->data;
+
+    $schema = json_decode(file_get_contents(__DIR__ . './../../schema.json'), true);
+
+    $validation = \rest_validate_value_from_schema($json, $schema);
+    $this->assertTrue($validation, 'data returned by loop endpoint matches schema');
   }
 }
