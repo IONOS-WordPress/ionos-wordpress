e2e53e7c9064f333515fecfbbd14adf38c8eef5a
Denise Stamatov <denise.stamatov@ionos.com>
2025-09-11 15:18:02 +0200

    make translatable

--- packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/blocks/site-health/index.php
+++ packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/blocks/site-health/index.php
@@ -43,7 +43,7 @@ function render_callback(): void
             </div>
             <div class="ionos-site-health-overview__info-items">
             <div class="ionos-site-health-overview__info-item site-health-status">
-              <p><?php \esc_html__('Site health', 'ionos-essentials')?></p>
+              <p><?php \esc_html_e('Site health', 'ionos-essentials')?></p>
               <strong id="site-health-status-text" >
                 <div class="site-health-status-circle">
                   <svg aria-hidden="true" focusable="false" width="100%" height="100%" viewBox="0 0 200 200" version="1.1" xmlns="http://www.w3.org/2000/svg">
@@ -51,17 +51,17 @@ function render_callback(): void
                     <circle id="bar" r="90" cx="100" cy="100" fill="transparent" stroke-dasharray="565.48" stroke-dashoffset="0"></circle>
                 </svg>
                 </div>
-                <span id="site-health-status-message" class="site-health-color"><?php echo esc_html_e('Results are still loading&hellip;'); ?></span>
+                <span id="site-health-status-message" class="site-health-color"><?php echo \esc_html_e('Results are still loading&hellip;'); ?></span>
               </strong>
             </div>
 
 
               <div class="ionos-site-health-overview__info-item">
-                <h3 class="ionos-site-health-overview__info-item-title"><?php \esc_html__('WordPress version', 'ionos-essentials')?></h3>
+                <h3 class="ionos-site-health-overview__info-item-title"><?php \esc_html_e('WordPress version', 'ionos-essentials')?></h3>
                 <h4 class="headline headline--sub"><?php echo \get_bloginfo('version'); ?></h4>
               </div>
               <div class="ionos-site-health-overview__info-item">
-                <h3 class="ionos-site-health-overview__info-item-title"><?php \esc_html__('PHP version', 'ionos-essentials')?></h3>
+                <h3 class="ionos-site-health-overview__info-item-title"><?php \esc_html_e('PHP version', 'ionos-essentials')?></h3>
                 <h4 class="headline headline--sub"><?php echo PHP_VERSION; ?></h4>
               </div>
             </div>
