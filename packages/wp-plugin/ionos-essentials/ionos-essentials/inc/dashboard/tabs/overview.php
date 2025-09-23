67ef68f154962a6fc60bdac4d5ad099c62052f0f
Thomas Rose <thomas.rose@ionos.com>
2025-09-23 10:37:52 +0200

    fix: add small screen responsiveness to next best actions and my account sections

--- packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/tabs/overview.php
+++ packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/tabs/overview.php
@@ -13,8 +13,8 @@ defined('ABSPATH') || exit();
     -->
     <div class="grid-col grid-col--12"><?php blocks\site_health\render_callback(); ?></div>
 
-    <div class="grid-col grid-col--8"><?php blocks\next_best_actions\render(); ?></div>
-    <div class="grid-col grid-col--4">
+    <div class="grid-col grid-col--8 grid-col--small-12"><?php blocks\next_best_actions\render_callback(); ?></div>
+    <div class="grid-col grid-col--4 grid-col--small-12">
       <?php blocks\quick_links\render_callback(); ?>
       <?php blocks\my_account\render_callback(); ?>
     </div>
