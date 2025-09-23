7660c141b9ee2b228b84436936d3c3ce79264731
Denise Stamatov <denise.stamatov@ionos.com>
2025-09-23 13:34:40 +0200

    revert changes

--- packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/tabs/overview.php
+++ packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/tabs/overview.php
@@ -12,8 +12,9 @@ defined('ABSPATH') || exit();
       but empty cells should not be displayed (see)
     -->
     <div class="grid-col grid-col--12"><?php blocks\site_health\render_callback(); ?></div>
-    <div class="grid-col grid-col--8 grid-col--small-12 ionos-next-best-action"><?php blocks\next_best_actions\render(); ?></div>
-    <div class="grid-col grid-col--4 grid-col--small-12 ionos-quick-links">
+
+    <div class="grid-col grid-col--8 grid-col--small-12"><?php blocks\next_best_actions\render(); ?></div>
+    <div class="grid-col grid-col--4 grid-col--small-12">
       <?php blocks\quick_links\render_callback(); ?>
       <?php blocks\my_account\render_callback(); ?>
     </div>
