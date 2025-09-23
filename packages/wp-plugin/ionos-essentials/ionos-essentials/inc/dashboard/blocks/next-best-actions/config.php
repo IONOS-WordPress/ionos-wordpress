3e4c0eba3e8c3464ab61ab65d7e415a6fe4585ce
peperuda-dev <denise.stamatov@ionos.com>
2025-09-23 10:34:02 +0200

    Feature/gphwpp 4094 use correct icons (#541)
    * add new icons and refactor code

--- packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/blocks/next-best-actions/config.php
+++ packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/blocks/next-best-actions/config.php
@@ -215,7 +215,7 @@ NBA::register(
   completed: false,
   dismiss_on_click: true,
   categories: ['always'],
-  icon: 'absent-16'
+  icon: 'megaphone'
 );
 
 if ('ionos' === Tenant::get_slug()) {
@@ -228,6 +228,6 @@ if ('ionos' === Tenant::get_slug()) {
     completed: false,
     dismiss_on_click: true,
     categories: ['always'],
-    icon: 'accounting-48'
+    icon: 'conversation'
   );
 }
