d36d09b002f1466d2e12d90c48472289281cb22e
Thomas Rose <thomas.rose@ionos.com>
2025-09-23 10:45:15 +0200

    typo

--- packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/tabs/overview.php
+++ packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/tabs/overview.php
@@ -13,7 +13,7 @@ defined('ABSPATH') || exit();
     -->
     <div class="grid-col grid-col--12"><?php blocks\site_health\render_callback(); ?></div>
 
-    <div class="grid-col grid-col--8 grid-col--small-12"><?php blocks\next_best_actions\render_callback(); ?></div>
+    <div class="grid-col grid-col--8 grid-col--small-12"><?php blocks\next_best_actions\render(); ?></div>
     <div class="grid-col grid-col--4 grid-col--small-12">
       <?php blocks\quick_links\render_callback(); ?>
       <?php blocks\my_account\render_callback(); ?>
