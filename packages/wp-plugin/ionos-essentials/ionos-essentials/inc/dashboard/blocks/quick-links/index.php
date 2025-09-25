c6a9a7f3c03c059dc2663111db2fcd27fda9f756
Thomas Rose <thomas.rose@ionos.com>
2025-09-25 17:05:36 +0200

    add event listener

--- packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/blocks/quick-links/index.php
+++ packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/blocks/quick-links/index.php
@@ -20,8 +20,9 @@ function render_callback(): void
         <?php
               foreach (get_config() as $link) {
                 printf(
-                  '<a href="%s"><i class="button__icon exos-icon exos-icon-%s"></i>%s</a>',
+                  '<a href="%s" data-track-link="%s"><i class="button__icon exos-icon exos-icon-%s"></i>%s</a>',
                   \esc_url($link['url']),
+                  \esc_attr($link['text']),
                   $link['icon'],
                   \esc_html($link['text'])
                 );
--- packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/dashboard.js
+++ packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/dashboard.js
@@ -140,6 +140,8 @@ document.addEventListener('DOMContentLoaded', function () {
         }
       }, 800);
     });
+
+    ionos_loop_track_click(target.dataset.nbaId + '/' + status)
   };
 
   dashboard.querySelector('#ionos_essentials_install_gml')?.addEventListener('click', function (event) {
@@ -341,7 +343,15 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 
- function track_click(anchor = "foo") {
+
+  dashboard.querySelectorAll('[data-track-link]').forEach((element) => {
+    element.addEventListener('click', (event) => {
+      ionos_loop_track_click(element.dataset.trackLink);
+    });
+  });
+
+
+ function ionos_loop_track_click(anchor) {
     fetch(wpData.restUrl + 'ionos/essentials/loop/v1/click', {
       method: 'POST',
       headers: {
