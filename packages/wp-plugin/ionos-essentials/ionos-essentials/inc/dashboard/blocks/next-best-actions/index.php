df24ac9d47f9a0e4e12e44ca6c8ed483ca7a8bcc
Thomas Rose <thomas.rose@ionos.com>
2025-09-25 20:28:45 +0200

    add always category

--- packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/blocks/next-best-actions/index.php
+++ packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/blocks/next-best-actions/index.php
@@ -58,7 +58,7 @@ function render(): void
     return;
   }
 
-  \update_option('ionos_loop_nba_actions_shown', array_keys($args['actions']));
+  \update_option('ionos_loop_nba_actions_shown', array_merge(array_keys($args['actions']), array_keys($args['always_actions'] ?? [])));
 
   main_view($args);
 }
