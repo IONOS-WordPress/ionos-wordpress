1d4461c4baf6da9206964c5b567c58394cfd384c
Thomas Rose <thomas.rose@ionos.com>
2025-10-07 19:29:14 +0200

    adapt schema

--- packages/wp-plugin/ionos-essentials/ionos-essentials/inc/loop/rest-callback.php
+++ packages/wp-plugin/ionos-essentials/ionos-essentials/inc/loop/rest-callback.php
@@ -15,26 +15,8 @@ function _rest_loop_callback(): \WP_REST_Response
 
   \add_option(IONOS_LOOP_DATACOLLECTOR_LAST_ACCESS, time());
 
-  // $core_data = [
-  //   'generic'       => _get_generic(),
-  //   'user'          => \count_users('memory'),
-  //   'active_theme'  => _get_themes(),
-  //   'active_plugins'=> _get_plugins(),
-  //   'posts'         => _get_posts_and_pages(),
-  //   'comments'      => _get_comments(),
-  //   'events'        => \get_option(IONOS_LOOP_EVENTS_OPTION, []),
-  //   'uploads'       => _get_uploads(),
-  //   'essentials'    => [
-  //     'dashboard'   => _get_dashbord_data(),
-  //     'security'    => \get_option(\ionos\essentials\security\IONOS_SECURITY_FEATURE_OPTION, []),
-  //     'maintenance' => \ionos\essentials\maintenance_mode\is_maintenance_mode(),
-  //   ],
-  //   'clicks'        => \get_option(IONOS_LOOP_CLICKS_OPTION, []),
-  //   'extendify'     => \get_option('extendify_onboarding_completed', null),
-  // ];
-
   $core_data = [
-    'generic'       => _get_generic(),
+    'hosting'       => _get_hosting(),
     'wordpress'     => [
       'user'                => \count_users('memory'),
       'active_theme'        => _get_themes(),
@@ -48,9 +30,6 @@ function _rest_loop_callback(): \WP_REST_Response
       'siteurl'             => \get_option('siteurl', ''),
       'home'                => \get_option('home', ''),
     ],
-
-    'extendify'     => \get_option('extendify_onboarding_completed', null),
-
     'events'        => \get_option(IONOS_LOOP_EVENTS_OPTION, []),
     'clicks'        => \get_option(IONOS_LOOP_CLICKS_OPTION, []),
 
@@ -101,7 +80,7 @@ function _get_dashbord_data(): array
   ];
 }
 
-function _get_generic(): array
+function _get_hosting(): array
 {
   return [
     'locale'              => \get_locale(),
@@ -110,12 +89,8 @@ function _get_generic(): array
     'tenant'              => Tenant::get_slug(),
     'core_version'        => \get_bloginfo('version'),
     'php_version'         => PHP_VERSION,
-    // 'installed_themes'    => count(\wp_get_themes()),
-    // 'installed_plugins'   => count(\get_plugins()),
+    'extendify'           => (bool) \get_option('extendify_onboarding_completed', null),
     'instance_created'    => _get_instance_creation_date(),
-    // 'permalink_structure' => \get_option('permalink_structure', ''),
-    // 'siteurl'             => \get_option('siteurl', ''),
-    // 'home'                => \get_option('home', ''),
   ];
 }
 
