3553ad2e7673e39c806e2df710851240a52a713c
Denise Stamatov <denise.stamatov@ionos.com>
2025-10-08 16:47:50 +0200

    load script only in theme overview

--- packages/wp-plugin/ionos-essentials/ionos-essentials/src/wpscan/theme-overview.js
+++ packages/wp-plugin/ionos-essentials/ionos-essentials/src/wpscan/theme-overview.js
@@ -4,37 +4,42 @@ console.log(foo);
 // tell eslint that the global variable exists when this file gets executed
 /* global ionosWPScanThemes:true */
 document.addEventListener('DOMContentLoaded', function () {
-  function highlightThemes() {
-    const themes = document.querySelectorAll('[data-slug]');
-    if (themes.length === 0) {
-      // Retry after a short delay if no themes are found
-      // I don't know why this is needed, but the themes are not loaded yet
-      window.setTimeout(highlightThemes, 10);
-      return;
-    }
-
-    themes.forEach(function (theme) {
-      if (!ionosWPScanThemes.slugs.includes(theme.dataset.slug)) {
+  
+  const parent = document.querySelector('.theme-browser');
+  
+  if (parent) {
+    function highlightThemes() {
+      const themes = document.querySelectorAll('[data-slug]');
+      if (themes.length === 0) {
+        // Retry after a short delay if no themes are found
+        // I don't know why this is needed, but the themes are not loaded yet
+        window.setTimeout(highlightThemes, 10);
         return;
       }
 
-      const html = `<p>${ionosWPScanThemes.i18n.issues_found}. <span class="ionos-no-activation">${ionosWPScanThemes.i18n.no_activation}</span> <a href="admin.php?page=${ionosWPScanThemes.i18n.brand}#tools" class="" type="button">${ionosWPScanThemes.i18n.more_info}</a></p>`;
-      // Prevent link click from bubbling
-      setTimeout(() => {
-        const link = theme.querySelector('a[href="admin.php?page=' + ionosWPScanThemes.i18n.brand + '#tools"]');
-        if (link) {
-          link.addEventListener('click', function (e) {
-            e.stopPropagation();
-          });
+      themes.forEach(function (theme) {
+        if (!ionosWPScanThemes.slugs.includes(theme.dataset.slug)) {
+          return;
         }
-      }, 0);
 
-      const notice = document.createElement('div');
-      notice.innerHTML = html;
-      notice.classList.add('update-message', 'notice', 'inline', 'notice-warning', 'notice-alt', 'ionos-theme-issues');
-      theme.appendChild(notice);
-    });
+        const html = `<p>${ionosWPScanThemes.i18n.issues_found}. <span class="ionos-no-activation">${ionosWPScanThemes.i18n.no_activation}</span> <a href="admin.php?page=${ionosWPScanThemes.i18n.brand}#tools" class="" type="button">${ionosWPScanThemes.i18n.more_info}</a></p>`;
+        // Prevent link click from bubbling
+        setTimeout(() => {
+          const link = theme.querySelector('a[href="admin.php?page=' + ionosWPScanThemes.i18n.brand + '#tools"]');
+          if (link) {
+            link.addEventListener('click', function (e) {
+              e.stopPropagation();
+            });
+          }
+        }, 0);
+
+        const notice = document.createElement('div');
+        notice.innerHTML = html;
+        notice.classList.add('update-message', 'notice', 'inline', 'notice-warning', 'notice-alt', 'ionos-theme-issues');
+        theme.appendChild(notice);
+      });
+    }
+
+    highlightThemes();
   }
-
-  highlightThemes();
 });
