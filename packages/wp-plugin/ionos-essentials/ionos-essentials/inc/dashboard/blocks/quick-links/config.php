a57de2f977eb5642f57b8d4ab45ca61b55909f5b
Lars Gersmann <lars.gersmann@ionos.com>
2025-10-13 18:00:35 +0200

    fix(loop) : use non localized ids for click events

--- packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/blocks/quick-links/config.php
+++ packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/blocks/quick-links/config.php
@@ -11,6 +11,7 @@ function get_config(): array
   return [
     [
       'url'  => $blog_url . '/wp-admin/post-new.php?post_type=page',
+      'id'  => 'add-new-page',
       'text' => __(
         'Add new page',
         'ionos-essentials'
@@ -19,11 +20,13 @@ function get_config(): array
     ],
     [
       'url'  => $blog_url . '/wp-admin/post-new.php',
+      'id'  => 'add-new-post',
       'text' => __('Add new post', 'ionos-essentials'),
       'icon' => 'helparticle-16',
     ],
     [
       'url'  => $blog_url . '/wp-admin/site-editor.php?p=%2Fnavigation',
+      'id'  => 'edit-site-navigation',
       'text' => __(
         'Edit site Navigation',
         'ionos-essentials'
@@ -32,6 +35,7 @@ function get_config(): array
     ],
     [
       'url'  => $blog_url . '/wp-admin/site-editor.php?path=%2Fwp_global_styles',
+      'id'  => 'change-styles',
       'text' => __(
         'Change Styles',
         'ionos-essentials'
@@ -40,11 +44,13 @@ function get_config(): array
     ],
     [
       'url'  => $blog_url . '/wp-admin/site-editor.php?p=%2Fpage&canvas=edit&ionos-siteeditor-quick-link=header',
+      'id'  => 'edit-header',
       'text' => __('Edit Header', 'ionos-essentials'),
       'icon' => 'website-48',
     ],
     [
       'url'  => $blog_url . '/wp-admin/site-editor.php?p=%2Fpage&canvas=edit&ionos-siteeditor-quick-link=footer',
+      'id'  => 'edit-footer',
       'text' => __(
         'Edit Footer',
         'ionos-essentials'
@@ -53,6 +59,7 @@ function get_config(): array
     ],
     [
       'url'  => $blog_url . '/wp-admin/plugin-install.php',
+      'id'  => 'add-plugins',
       'text' => __(
         'Add Plugins',
         'ionos-essentials'
@@ -61,6 +68,7 @@ function get_config(): array
     ],
     [
       'url'  => $blog_url . '/wp-admin/upload.php',
+      'id'  => 'upload-media',
       'text' => __('Upload Media files', 'ionos-essentials'),
       'icon' => 'upload-16',
     ],
