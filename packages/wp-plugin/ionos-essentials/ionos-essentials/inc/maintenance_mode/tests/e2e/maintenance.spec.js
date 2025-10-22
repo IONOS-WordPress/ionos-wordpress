3c632fc88ba417f22de125a0977d1d13169304de
Lars Gersmann <lars.gersmann@ionos.com>
2025-10-22 20:20:32 +0200

    fix(e2e) : maintenance test is now self contained and can be runned independant
    - add test groups
    - added titles that make sense
    - normalization: tests are always sourrounded by a describe
    -  make wp-cli calls quiet

--- packages/wp-plugin/ionos-essentials/ionos-essentials/inc/maintenance_mode/tests/e2e/maintenance.spec.js
+++ packages/wp-plugin/ionos-essentials/ionos-essentials/inc/maintenance_mode/tests/e2e/maintenance.spec.js
@@ -1,31 +1,54 @@
 import { test, expect } from '@wordpress/e2e-test-utils-playwright';
 import { execTestCLI } from '../../../../../../../../playwright/wp-env';
 
-test.describe('Maintenance', () => {
-  test.beforeAll(async () => {
-    execTestCLI(`wp option delete ionos_essentials_maintenance_mode`);
-  });
+test.describe(
+  'essentials:dashboard maintenance',
+  {
+    tag: ['@dashboard', '@maintenance'],
+  },
+  () => {
+    test.beforeAll(async () => {
+      execTestCLI(`
+      # set popup after timestamp to a far future date to prevent popups during e2e tests
+      wp --quiet user meta update 1 ionos_popup_after_timestamp ${Math.MAX_SAFE_INTEGER}
+      # set essentials welcome overlay already clicked away
+      wp --quiet user meta update 1 ionos_essentials_welcome true
+      # simulate extendify onboarding already done
+      wp --quiet option update extendify_attempted_redirect_count 4
+      # reset maintenance mode
+      wp --quiet option delete ionos_essentials_maintenance_mode
+    `);
+    });
 
-  test.afterAll(async () => {
-    execTestCLI(`wp option delete ionos_essentials_maintenance_mode`);
-  });
+    test.afterAll(async () => {
+      execTestCLI(`wp --quiet option delete ionos_essentials_maintenance_mode`);
+    });
 
-  test('maintenance mode is enabled', async ({ admin, page }) => {
-    await admin.visitAdminPage('/');
-    await page.getByRole('button', { name: 'Tools' }).click();
-    await page.locator('#ionos_essentials_maintenance_mode').click();
+    test('maintenance mode is enabled', async ({ admin, page }) => {
+      await admin.visitAdminPage('/');
+      await page.getByRole('button', { name: 'Tools' }).click();
+      await page.locator('#ionos_essentials_maintenance_mode').click();
 
-    // Still no redirection to the maintenance page, because we are logged in
-    await page.goto('/');
-    let body = await page.locator('body');
-    await expect(body).toHaveText(/Blog/);
+      // Still no redirection to the maintenance page, because we are logged in
+      await page.goto('/');
+      let body = await page.locator('body');
+      await expect(body).toHaveText(/Blog/);
 
-    await page.goto('/wp-login.php?action=logout');
-    await page.click('text=log out');
+      await page.goto('/wp-login.php?action=logout');
+      await page.click('text=log out');
 
-    await page.goto('/');
-    body = await page.locator('body');
-    await page.waitForTimeout(1000);
-    await expect(page).toHaveTitle('Construction');
-  });
-});
+      await page.goto('/');
+      body = await page.locator('body');
+      await page.waitForTimeout(1000);
+      await expect(page).toHaveTitle('Construction');
+    });
+  }
+);
+
+// test('maintenance mode is enabled', async ({ admin, page }) => {
+//   // await globalThis.requestUtils.setupRest();
+
+//   await admin.visitAdminPage('/');
+//   await page.getByRole('button', { name: 'Tools' }).click();
+//   await page.locator('#ionos_essentials_maintenance_mode').click();
+// });
