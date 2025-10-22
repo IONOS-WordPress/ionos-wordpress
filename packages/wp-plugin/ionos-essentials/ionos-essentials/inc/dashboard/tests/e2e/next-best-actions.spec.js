f7582f6262b6162a6e2b6183bd4ca37b4e839337
Lars Gersmann <lars.gersmann@ionos.com>
2025-10-22 17:56:44 +0200

    fix(e2e) : nba test can be executed again and again

--- packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/tests/e2e/next-best-actions.spec.js
+++ packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/tests/e2e/next-best-actions.spec.js
@@ -4,8 +4,11 @@ import { execTestCLI } from '../../../../../../../../playwright/wp-env';
 test.describe('essentials:dashboard next-best-actions block', () => {
   test.beforeAll(async () => {
     execTestCLI(`
-      wp option delete ionos_nba_status
+      # reset nba options
+      wp option delete ionos_nba_status ionos_essentials_nba_setup_completed ionos_essentials_loop_nba_actions_shown
+      # set essentials welcome overlay already clicked away
       wp user meta update 1 ionos_essentials_welcome true
+      # simulate extendify onboarding already done
       wp option update extendify_attempted_redirect_count 4
     `);
   });
