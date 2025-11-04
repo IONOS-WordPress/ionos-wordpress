65de0d7d6af2aa118ba61ea0498fb463dfb958a3
Lars Gersmann <lars.gersmann@ionos.com>
2025-11-04 11:14:13 +0100

    fix(eslint) : current eslint version doesnt resolve installed package @storybook/react. As a workaround we disable the eslint resolve rule for now

--- playwright/index.jsx
+++ playwright/index.jsx
@@ -1,3 +1,5 @@
+// TODO: Remove the eslint-disable-next-line when eslint is able to resolve @storybook/react
+// eslint-disable-next-line import/no-unresolved
 import { setProjectAnnotations } from '@storybook/react';
 import previewAnnotations from '../.storybook/preview';
 
