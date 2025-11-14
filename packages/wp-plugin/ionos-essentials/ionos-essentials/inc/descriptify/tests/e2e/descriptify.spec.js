557dc1f4499ff8dd7a7008c21363e48a09674bcb
Thomas Rose <thomas.rose@ionos.com>
2025-11-14 10:40:45 +0100

    set market wihtin test

--- packages/wp-plugin/ionos-essentials/ionos-essentials/inc/descriptify/tests/e2e/descriptify.spec.js
+++ packages/wp-plugin/ionos-essentials/ionos-essentials/inc/descriptify/tests/e2e/descriptify.spec.js
@@ -1,7 +1,9 @@
 import { test, expect } from '@wordpress/e2e-test-utils-playwright';
+import { execTestCLI } from '../../../../../../../../playwright/wp-env';
 
 test.describe('Descriptify', () => {
   test('Changing of homeurl is disabled', async ({ admin, page }) => {
+    execTestCLI(`wp option update ionos_market de`);
     await admin.visitAdminPage('/options-general.php');
 
     await expect(page.locator('#siteurl')).toBeDisabled();
