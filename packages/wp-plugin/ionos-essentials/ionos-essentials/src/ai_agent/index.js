fd8cf9a4794c2c3990f9a94704d894f0c21a414e
Tobias <tobias.bode@ionos.com>
2025-11-17 11:28:37 +0100

    refactor(nba): code review

--- packages/wp-plugin/ionos-essentials/ionos-essentials/src/ai_agent/index.js
+++ packages/wp-plugin/ionos-essentials/ionos-essentials/src/ai_agent/index.js
@@ -1,28 +1,22 @@
-const interval = setInterval(() => {
-  const agentItem = document.querySelector('.extendify-agent');
-  if (!agentItem) {
-    return;
-  }
-
-  const innerButton = agentItem.querySelector('button, a');
-  if (!innerButton) {
-    clearInterval(interval);
-    return;
-  }
-
-  const isDisabled = innerButton.classList.contains('opacity-60');
-  const urlParams = new URLSearchParams(window.location.search);
-  const highlight = urlParams.get('ionos-highlight');
-
-  if (!isDisabled && highlight) {
-    if (window.jQuery) {
-      window.jQuery(innerButton).trigger('click');
-    } else {
-      // fallback for jQuery: Clicking via mouse event
-      const event = new MouseEvent('click', { bubbles: true, cancelable: true });
-      innerButton.dispatchEvent(event);
+if (new URLSearchParams(window.location.search).get('ionos-highlight') === 'chatbot') {
+  const interval = setInterval(() => {
+    const agentItem = document.querySelector('.extendify-agent');
+    if (!agentItem) {
+      return;
     }
-  }
 
-  clearInterval(interval);
-}, 500);
+    const innerButton = agentItem.querySelector('button, a');
+    if (!innerButton) {
+      clearInterval(interval);
+      return;
+    }
+
+    const isDisabled = innerButton.classList.contains('opacity-60');
+
+    if (!isDisabled) {
+      innerButton.click();
+    }
+
+    clearInterval(interval);
+  }, 500);
+}
