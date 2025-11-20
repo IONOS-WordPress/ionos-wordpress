cf70ba171922d250b4d0c63d5701d379beed537f
Lars Gersmann <lars.gersmann@ionos.com>
2025-11-20 19:03:57 +0100

    fixed(e2e) : added tags to playwright test

--- packages/wp-plugin/ionos-essentials/ionos-essentials/inc/switch-page/tests/e2e/switch-page.spec.js
+++ packages/wp-plugin/ionos-essentials/ionos-essentials/inc/switch-page/tests/e2e/switch-page.spec.js
@@ -1,19 +1,25 @@
 import { test, expect } from '@wordpress/e2e-test-utils-playwright';
 
-test.describe('Switch page', () => {
-  test('showing correct links', async ({ admin, page }) => {
-    const errors = [];
-    page.on('console', (msg) => {
-      if (msg.type() === 'error') {
-        errors.push(msg.text());
-      }
+test.describe(
+  'Switch page',
+  {
+    tag: ['@dashboard', '@switchpage'],
+  },
+  () => {
+    test('showing correct links', async ({ admin, page }) => {
+      const errors = [];
+      page.on('console', (msg) => {
+        if (msg.type() === 'error') {
+          errors.push(msg.text());
+        }
+      });
+
+      await admin.visitAdminPage('/admin.php?page=ionos-onboarding');
+
+      await expect(page.locator('a[href*="extendify-launch"]')).toBeVisible();
+      await expect(page.getByRole('link', { name: 'Create manually' })).toBeVisible();
+      // .toHaveLength(0) is best practice according to playwright docs
+      await expect(errors).toHaveLength(0);
     });
-
-    await admin.visitAdminPage('/admin.php?page=ionos-onboarding');
-
-    await expect(page.locator('a[href*="extendify-launch"]')).toBeVisible();
-    await expect(page.getByRole('link', { name: 'Create manually' })).toBeVisible();
-    // .toHaveLength(0) is best practice according to playwright docs
-    await expect(errors).toHaveLength(0);
-  });
-});
+  }
+);
