1c925872987b1b154f0e61c8af8dfe82c6c13a63
Lars Gersmann <lars.gersmann@ionos.com>
2025-11-20 18:30:54 +0100

    fixed(e2e) : added tags to playwright test

--- packages/wp-plugin/ionos-essentials/ionos-essentials/inc/login/tests/e2e/login.spec.js
+++ packages/wp-plugin/ionos-essentials/ionos-essentials/inc/login/tests/e2e/login.spec.js
@@ -1,19 +1,25 @@
 import { test, expect } from '@wordpress/e2e-test-utils-playwright';
 
-test.describe('Login page', () => {
-  test('showing brand logo', async ({ page }) => {
-    const errors = [];
-    page.on('console', (msg) => {
-      if (msg.type() === 'error') {
-        errors.push(msg.text());
-      }
+test.describe(
+  'Login page',
+  {
+    tag: ['@dashboard', '@login'],
+  },
+  () => {
+    test('showing brand logo', async ({ page }) => {
+      const errors = [];
+      page.on('console', (msg) => {
+        if (msg.type() === 'error') {
+          errors.push(msg.text());
+        }
+      });
+
+      await page.goto('/wp-login.php');
+
+      await expect(page.locator('img[src*="ionos.svg"]')).toBeVisible();
+
+      // Make sure there are no console errors. This is to catch any issues with loading the logo.
+      await expect(errors).toEqual([]);
     });
-
-    await page.goto('/wp-login.php');
-
-    await expect(page.locator('img[src*="ionos.svg"]')).toBeVisible();
-
-    // Make sure there are no console errors. This is to catch any issues with loading the logo.
-    await expect(errors).toEqual([]);
-  });
-});
+  }
+);
