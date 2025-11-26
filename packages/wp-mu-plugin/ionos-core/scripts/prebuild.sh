19a13d10dc2b4e6f4f6d4420ec6fbb1145275d78
Lars Gersmann <lars.gersmann@ionos.com>
2025-11-26 11:28:58 +0100

    fix(prebuild): prevent script execution in CI environments

--- packages/wp-mu-plugin/ionos-core/scripts/prebuild.sh
+++ packages/wp-mu-plugin/ionos-core/scripts/prebuild.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [[ "$CI" == "true" ]]; then
+  echo "This script is not intended to be run in CI environments."
+  exit 0
+fi
+
 readonly PLUGINS_TO_INSTALL=(
   https://downloads.wordpress.org/plugin/wordpress-seo.zip
 )
