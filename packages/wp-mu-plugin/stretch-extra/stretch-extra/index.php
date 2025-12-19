6ddcc310c9fd222ca0981c3e658486448a4e66d9
Lars Gersmann <lars.gersmann@ionos.com>
2025-12-19 06:49:21 +0100

    refactor(stretch-extra): remove dynamic loading of PHP files from inc directory

--- packages/wp-mu-plugin/stretch-extra/stretch-extra/index.php
+++ packages/wp-mu-plugin/stretch-extra/stretch-extra/index.php
@@ -10,45 +10,7 @@ defined('ABSPATH') || exit();
 
 const IONOS_CUSTOM_DIR  = __DIR__;
 
-/*
-   require_once
-   - all PHP files
-   - and directories with index.php
-   from inc directory
-*/
-
-const MU_FEATURES = __DIR__ . '/inc';
-
-if (!is_dir(MU_FEATURES)) {
-  error_log(
-    sprintf(
-      'skip loading mu plugin features : directory(=%s) does not exist',
-       MU_FEATURES
-    )
-  );
-  return;
-}
-
-$items = scandir(MU_FEATURES);
-
-foreach ($items as $item) {
-  if ($item === '.' || $item === '..') {
-    continue;
-  }
-
-  $item_path = MU_FEATURES . '/' . $item;
-
-  // If it's a PHP file, require it
-  if (is_file($item_path) && pathinfo($item_path, PATHINFO_EXTENSION) === 'php') {
-    require_once $item_path;
-  }
-
-  // If it's a directory containing index.php, require the index.php
-  elseif (is_dir($item_path)) {
-    $index_file = $item_path . '/index.php';
-    if (file_exists($index_file)) {
-      require_once $index_file;
-    }
-  }
-}
+require_once __DIR__ . '/inc/migration.php';
+require_once __DIR__ . '/inc/secondary-plugin_dir';
+require_once __DIR__ . '/inc/secondary-theme_dir';
 
