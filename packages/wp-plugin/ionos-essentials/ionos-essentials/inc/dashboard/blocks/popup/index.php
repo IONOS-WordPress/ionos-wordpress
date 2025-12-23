dc81a1f07c3a05254dcf98239cc20e8c878e91f0
Lars Gersmann <lars.gersmann@ionos.com>
2025-12-23 16:52:19 +0100

    chore: refactor render_callback in popup block for improved readability and maintainability

--- packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/blocks/popup/index.php
+++ packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/blocks/popup/index.php
@@ -8,7 +8,6 @@ defined('ABSPATH') || exit();
 
 function render_callback(): void
 {
-
   if ('ionos' !== Tenant::get_slug()) {
     return;
   }
@@ -23,39 +22,45 @@ function render_callback(): void
   if (empty(\get_user_meta(\get_current_user_id(), 'ionos_essentials_welcome', true))) {
     return;
   }
-  ?>
-<dialog id="ionos-essentials-popup" class="ionos-essentials-popup" open>
-  <div class="dialog__content">
-    <div class="horizontal-card popup-card">
 
-        <div class="horizontal-card__content">
-            <section class="horizontal-card__section" style="flex-grow: 0;">
+  $headline  = \esc_html__('Your Feedback is important to us', 'ionos-essentials');
+  $paragraph = \esc_html__(
+    'We\'re always looking for ways to make your WordPress hosting experience even better. Please take a few minutes to fill out a quick online survey.',
+    'ionos-essentials'
+  );
+  $close_button_text  = \esc_html__('Close', 'ionos-essentials');
+  $survey_button_text = \esc_html__('Take the survey', 'ionos-essentials');
+  $survey_url         = \esc_attr(\ionos\essentials\dashboard\blocks\whatsnew\get_survey_url());
 
-                <h2 class="headline">
-                    <?php \esc_html_e('Your Feedback is important to us', 'ionos-essentials'); ?>
-                </h2>
-                 <p class="paragraph">
-                  <?php \esc_html_e(
-                    'We\'re always looking for ways to make your WordPress hosting experience even better. Please take a few minutes to fill out a quick online survey.',
-                    'ionos-essentials'
-                  ); ?>
-                 </p>
+  printf(
+    <<<EOF
+    <dialog id="ionos-essentials-popup" class="ionos-essentials-popup" open>
+      <div class="dialog__content">
+        <div class="horizontal-card popup-card">
 
-             </section>
+            <div class="horizontal-card__content">
+                <section class="horizontal-card__section" style="flex-grow: 0;">
+
+                    <h2 class="headline">
+                        {$headline}
+                    </h2>
+                    <p class="paragraph">
+                      {$paragraph}
+                    </p>
+
+                </section>
+            </div>
+            <footer class="horizontal-card__footer horizontal-card__content--vertical-align-center" style="width: 100%;display: flex; justify-content: center;">
+                <button class="button button--secondary ionos-popup-dismiss">
+                    {$close_button_text}
+                </button>
+                <a href="{$survey_url}" class="button button--primary ionos-popup-dismiss" target="_blank">
+                  {$survey_button_text}
+                </a>
+            </footer>
         </div>
-        <footer class="horizontal-card__footer horizontal-card__content--vertical-align-center" style="width: 100%;display: flex; justify-content: center;">
-            <button class="button button--secondary ionos-popup-dismiss">
-                <?php \esc_html_e('Close', 'ionos-essentials'); ?>
-            </button>
-            <a href="<?php echo \esc_attr(
-              \ionos\essentials\dashboard\blocks\whatsnew\get_survey_url()
-            ); ?>" class="button button--primary ionos-popup-dismiss" target="_blank">
-              <?php \esc_html_e('Take the survey', 'ionos-essentials'); ?>
-            </a>
-        </footer>
-    </div>
-  </div>
-</dialog>
-
-<?php
+      </div>
+    </dialog>
+    EOF
+  );
 }
