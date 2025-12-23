af92dc75146cf0eb332399074b651bf378b5cef6
Lars Gersmann <lars.gersmann@ionos.com>
2025-12-23 16:30:46 +0100

    chore: refactor single_accordion_view for improved readability and structure

--- packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/blocks/next-best-actions/views/single-accordion-action.php
+++ packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/blocks/next-best-actions/views/single-accordion-action.php
@@ -7,32 +7,36 @@ use const ionos\essentials\PLUGIN_FILE;
 
 function single_accordion_view($action): void
 {
-  ?>
-
-<div id="<?php echo \esc_attr(
-  $action->id
-); ?>" class="panel__item <?php echo $action->expanded ? 'panel__item--expanded always-expanded ' : 'panel__item--closed expandable '; ?>
-  <?php echo esc_attr($action->active ? 'nba-active' : 'nba-inactive'); ?>"
-  aria-expanded="<?php echo $action->expanded ? 'true' : 'false'; ?>">
-  <header class="panel__item-header">
-    <div class="panel__icon">
-     <img src="<?php echo esc_url(plugins_url(
-       '/ionos-essentials/inc/dashboard/assets/' . $action->exos_icon . '.svg',
-       PLUGIN_FILE
-     )); ?>" alt="Icon" width="30" height="30">
-
-    </div>
-    <div class="panel__headline__container">
-      <h3 class="panel__headline">
-        <?php echo \esc_html($action->title); ?>
-      </h3>
-    </div>
-  </header>
-  <section class="panel__item-section">
-    <p class="paragraph"><?php echo \esc_html($action->description); ?></p>
-    <div><?php echo \wp_kses(create_buttons($action), 'post'); ?></div>
-  </section>
-</div>
-
-<?php
+  $action_id = \esc_attr($action->id);
+  $classes   = $action->expanded ? 'panel__item--expanded always-expanded ' : 'panel__item--closed expandable ';
+  $classes .= $action->active ? 'nba-active' : 'nba-inactive';
+  $aria_expanded = $action->expanded ? 'true' : 'false';
+  $exos_icon     = \esc_attr(plugins_url(
+    '/ionos-essentials/inc/dashboard/assets/' . $action->exos_icon . '.svg',
+    PLUGIN_FILE
+  ));
+  $action_title       = \esc_html($action->title);
+  $action_description = \esc_html($action->description);
+  $buttons            = \wp_kses(create_buttons($action), 'post');
+  printf(
+    <<<EOF
+  <div id="{$action_id}" class="panel__item {$classes}"
+    aria-expanded="{$aria_expanded}">
+    <header class="panel__item-header">
+      <div class="panel__icon">
+        <img src="{$exos_icon}" alt="Icon" width="30" height="30">
+      </div>
+      <div class="panel__headline__container">
+        <h3 class="panel__headline">
+          {$action_title}
+        </h3>
+      </div>
+    </header>
+    <section class="panel__item-section">
+      <p class="paragraph">{$action_description}</p>
+      <div>{$buttons}</div>
+    </section>
+  </div>
+  EOF
+  );
 }
