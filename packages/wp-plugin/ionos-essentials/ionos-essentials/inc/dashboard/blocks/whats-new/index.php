f287866fe5ed8dac1fc7ab22d5038d45df9e574c
Lars Gersmann <lars.gersmann@ionos.com>
2025-12-23 17:46:32 +0100

    chore: update survey URL retrieval and enhance rendering of the what's new section

--- packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/blocks/whats-new/index.php
+++ packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/blocks/whats-new/index.php
@@ -16,7 +16,7 @@ function get_survey_url(): string
     'es'    => 'https://feedback.ionos.com/nmdopgnfds?l=es',
     'it'    => 'https://feedback.ionos.com/nmdopgnfds?l=it',
   ];
-  $locale = determine_locale();
+  $locale = \determine_locale();
   if ($locale === 'en_US') {
     return $survey_links['en_us'];
   }
@@ -29,42 +29,56 @@ function get_survey_url(): string
 
 function render_callback(): void
 {
-  ?>
+  $image_url = \esc_url(\plugins_url('', __FILE__) . '/whats-new.png');
 
+  $headline  = \esc_html__("What's New", 'ionos-essentials');
+  $paragraph = \esc_html__('Get an overview of the latest updates and enhancements.', 'ionos-essentials');
+
+  // translators: %s is placeholder for the tenant name
+  $upgrade_headline = sprintf(
+    \esc_html__('Your %s Hub Just Got a Powerful Upgrade!', 'ionos-essentials'),
+    Tenant::get_label()
+  );
+  $upgrade_paragraph = \esc_html__(
+    "We've just rolled out a brand-new \"Tools & Security\" tab! All the features and user interfaces from your previous security plugin have now found their new home here, making everything more centralized and easier to manage. Plus, you'll find a new maintenance page function that you can switch on whenever you need it.",
+    'ionos-essentials'
+  );
+
+  $feedback_headline  = \esc_html__('Your Feedback is important to us', 'ionos-essentials');
+  $feedback_paragraph = \esc_html__(
+    'We\'re always looking for ways to make your WordPress hosting experience even better. Please take a few minutes to fill out a quick online survey.',
+    'ionos-essentials'
+  );
+
+  $survey_url       = \esc_attr(get_survey_url());
+  $survey_link_text = \esc_html__('Take the survey', 'ionos-essentials');
+
+  printf(
+    <<<EOF
 <div class="card">
-    <div class="card__content">
-      <section class="card__section">
-        <img src="<?php echo \esc_url(\plugins_url('', __FILE__) . '/whats-new.png'); ?>" style="float: right; height: 120px;">
+  <div class="card__content">
+    <section class="card__section">
+      <img src="{$image_url}" style="float: right; height: 120px;">
 
-        <h2 class="headline headline--sub"><?php \esc_html_e('What’s New', 'ionos-essentials'); ?></h2>
-        <p class="paragraph"><?php \esc_html_e('Get an overview of the latest updates and enhancements.', 'ionos-essentials'); ?></p>
+      <h2 class="headline headline--sub">{$headline}</h2>
+      <p class="paragraph">{$paragraph}</p>
 
-        <ul class="check-list">
-          <li>
-            <h3 class="headline headline--sub"><?php printf(
-              // translators: %s is placeholder for the tenant name
-              \esc_html__('Your %s Hub Just Got a Powerful Upgrade!', 'ionos-essentials'),
-              Tenant::get_label()
-            ); ?></h3>
-            <p><?php \esc_html_e(
-              "We've just rolled out a brand-new \"Tools & Security\" tab! All the features and user interfaces from your previous security plugin have now found their new home here, making everything more centralized and easier to manage. Plus, you'll find a new maintenance page function that you can switch on whenever you need it.",
-              'ionos-essentials'
-            ); ?></p>
-          </li>
-          <li>
-            <h3 class="headline headline--sub"><?php \esc_html_e('Your Feedback is important to us', 'ionos-essentials'); ?></h3>
-            <p><?php \esc_html_e(
-              'We\'re always looking for ways to make your WordPress hosting experience even better. Please take a few minutes to fill out a quick online survey.',
-              'ionos-essentials'
-            ); ?></p>
-            <a href="<?php echo \esc_attr(get_survey_url()); ?>" target="_blank" class="link link--action">
-              <?php \esc_html_e('Take the survey', 'ionos-essentials'); ?>
-            </a>
-          </li>
-        </ul>
-      </section>
-
-    </div>
+      <ul class="check-list">
+        <li>
+          <h3 class="headline headline--sub">{$upgrade_headline}</h3>
+          <p>{$upgrade_paragraph}</p>
+        </li>
+        <li>
+          <h3 class="headline headline--sub">{$feedback_headline}</h3>
+          <p>{$feedback_paragraph}</p>
+          <a href="{$survey_url}" target="_blank" class="link link--action">
+            {$survey_link_text}
+          </a>
+        </li>
+      </ul>
+    </section>
   </div>
-<?php
+</div>
+EOF
+  );
 }
