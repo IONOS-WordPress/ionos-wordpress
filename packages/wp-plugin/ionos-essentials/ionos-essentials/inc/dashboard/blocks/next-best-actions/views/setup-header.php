09d5c9e7865e4fbe3ba3906aa624751f607280e5
Lars Gersmann <lars.gersmann@ionos.com>
2025-12-23 16:18:40 +0100

    chore: refactor setup-header.php for improved readability and structure

--- packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/blocks/next-best-actions/views/setup-header.php
+++ packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/blocks/next-best-actions/views/setup-header.php
@@ -12,33 +12,34 @@ function render_setup_header($args): void
   $show_setup_actions = $args['show_setup_actions'] ?? false;
   $completed_actions  = $args['completed_actions']  ?? 0;
   $total_actions      = $args['total_actions']      ?? 0;
-  ?>
 
- <div class="headline"><?php \esc_html_e('🚀  Getting started with WordPress', 'ionos-essentials'); ?></div>
- <div class="container">
-    <div class="paragraph"><?php \esc_html_e(
-      'Ready to establish your online presence? Let\'s get the essentials sorted so your new site looks professional and is easy for people to find.',
-      'ionos-essentials'
-    ); ?></div>
-
-    <div style="width: 350px">
-      <div class="quotabar">
-        <div class="quotabar__bar quotabar__bar--small">
-          <span class="quotabar__value" style="width: <?php echo \esc_attr($completed_actions / $total_actions * 100); ?>%;"></span>
+  $headline = \esc_html_e('🚀  Getting started with WordPress', 'ionos-essentials');
+  $header   = \esc_html_e(
+    'Ready to establish your online presence? Let\'s get the essentials sorted so your new site looks professional and is easy for people to find.',
+    'ionos-essentials'
+  );
+  $completed_actions_percentage = \esc_attr($completed_actions / $total_actions * 100);
+  printf(
+    <<<EOL
+    <div class="headline">{$headline}</div>
+    <div class="container">
+      <div class="paragraph">{$header}</div>
+      <div style="width: 350px">
+        <div class="quotabar">
+          <div class="quotabar__bar quotabar__bar--small">
+            <span class="quotabar__value" style="width: {$completed_actions_percentage}%;"></span>
+          </div>
+            <p class="quotabar__text">
+              %s
+            </p>
+          </div>
         </div>
-          <p class="quotabar__text">
-            <?php
-              if ($completed_actions === $total_actions) {
-                \esc_html_e('All actions completed!', 'ionos-essentials');
-              } else {
-                // translators: 1: number of completed actions, 2: total number of actions
-                printf(__(' %1$d of %2$d completed', 'ionos-essentials'), $completed_actions, $total_actions);
-              }
-  ?>
-        </p>
-      </div>
-    </div>
-  </div>
-
-<?php
+      </div>  
+  EOL
+    ,
+    $completed_actions === $total_actions ?
+      \esc_html_e('All actions completed!', 'ionos-essentials') :
+      // translators: 1: number of completed actions, 2: total number of actions
+      printf(__(' %1$d of %2$d completed', 'ionos-essentials'), $completed_actions, $total_actions)
+  );
 }
