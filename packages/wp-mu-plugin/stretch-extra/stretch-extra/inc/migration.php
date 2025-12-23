d488234e0e3d25091b66e9c769c30838cc611997
Tobi Bode <12033092+tobibode@users.noreply.github.com>
2025-12-23 10:47:59 +0100

    Apply suggestion from @tobibode

--- packages/wp-mu-plugin/stretch-extra/stretch-extra/inc/migration.php
+++ packages/wp-mu-plugin/stretch-extra/stretch-extra/inc/migration.php
@@ -10,6 +10,7 @@ const IONOS_MIGRATION_OPTION = 'IONOS_MIGRATION_OPTION';
   if (\get_option(IONOS_MIGRATION_OPTION) === "1.0.0") {
     return;
   }
+// workaround only necessary until this is part of config API: https://hosting-jira.1and1.org/browse/GPHWPP-4254
   \wp_update_user([
     'ID'         => \get_current_user_id(),
     'user_email' => \get_option('admin_email')
