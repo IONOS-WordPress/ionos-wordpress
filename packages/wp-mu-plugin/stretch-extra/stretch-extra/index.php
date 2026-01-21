e2c9537fb76943b58ed4393956ac4fe757da1e07
Denise Stamatov <denise.stamatov@ionos.com>
2026-01-21 16:59:41 +0100

    remove debug code

--- packages/wp-mu-plugin/stretch-extra/stretch-extra/index.php
+++ packages/wp-mu-plugin/stretch-extra/stretch-extra/index.php
@@ -10,20 +10,16 @@ defined('ABSPATH') || exit();
 
 const IONOS_CUSTOM_DIR  = __DIR__;
 
-
 if (! defined('IONOS_IS_STRETCH')) {
-    echo("huhu1");
   define('IONOS_IS_STRETCH', strncmp(getcwd(), '/home/www/public', strlen('/home/www/public')) === 0);
 }
-echo("huhu2");
+
 // Set SFS server variable to fake stretch-extra context
 if (! defined('IONOS_IS_STRETCH_SFS') && defined('IONOS_IS_STRETCH')) {
-    echo("huhu3");
   define('IONOS_IS_STRETCH_SFS', array_key_exists('SFS', $_SERVER));
 }
-echo("huhu4");
+
 if (! defined('IONOS_IS_STRETCH_SFS')) {
-    echo("huhu5");
   $_SERVER['SFS'] = 'stretch-extra';
 }
 
