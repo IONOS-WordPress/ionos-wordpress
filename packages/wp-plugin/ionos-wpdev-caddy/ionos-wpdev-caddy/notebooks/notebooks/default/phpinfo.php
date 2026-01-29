f5b5782c0e2618afe839c69a6fca9fc00a46926d
Lars Gersmann <lars.gersmann@ionos.com>
2026-01-29 13:37:39 +0100

    fix: streamline whitespace cleanup in phpinfo output script

--- packages/wp-plugin/ionos-wpdev-caddy/ionos-wpdev-caddy/notebooks/notebooks/default/phpinfo.php
+++ packages/wp-plugin/ionos-wpdev-caddy/ionos-wpdev-caddy/notebooks/notebooks/default/phpinfo.php
@@ -18,10 +18,6 @@ $info = strip_tags($info);
 $info = html_entity_decode($info);
 
 // 4. Optional: Clean up excessive whitespace/newlines
-$info = preg_replace(
-  "/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/",
-  "\n",
-  $info
-);
+$info = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $info);
 
 echo $info;
