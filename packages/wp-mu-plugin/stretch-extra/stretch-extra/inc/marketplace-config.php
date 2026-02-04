2d7bb8c64d5ab3d755ad90cd802407ad4af66e3c
Thomas Rose <thomas.rose@ionos.com>
2026-02-04 10:56:54 +0100

    [skip ci] [wip] introduce marketplace config

---
+++ packages/wp-mu-plugin/stretch-extra/stretch-extra/inc/marketplace-config.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace ionos\stretch_extra;
+
+
+return [
+  'ionos_plugins' => [
+      'ionos-essentials' =>
+      [
+        'url' => 'https://github.com/IONOS-WordPress/ionos-wordpress/releases/download/%40ionos-wordpress%2Flatest/ionos-essentials-info.json'
+      ],
+  'ionos-sso' =>
+      [
+        'url' => 'https://s3-de-central.profitbricks.com/web-hosting/ionos-group/ionos-sso.info.json'
+      ],
+  'woocommerce-german-market-light' =>
+      [
+        'url' => 'https://s3-de-central.profitbricks.com/web-hosting/ionos-group/woocommerce-german-market-light.info.json'
+      ],
+    ],
+  'wordpress_org_plugins' =>
+    [
+      'antispam-bee',
+      'limit-login-attempts-reloaded',
+      'jetpack',
+      'broken-link-checker',
+      'contact-form-7',
+      'regenerate-thumbnails-advanced',
+      'embed-privacy',
+      'statify',
+      'lazy-loading-responsive-images',
+      'woocommerce',
+      'avatar-privacy',
+      'two-factor',
+      'webmention',
+      'podlove-podcasting-plugin-for-wordpress',
+      'wp-maintenance-mode',
+      'duplicate-post',
+      'google-analytics-for-wordpress',
+      'autoptimize'
+  ],
+];
