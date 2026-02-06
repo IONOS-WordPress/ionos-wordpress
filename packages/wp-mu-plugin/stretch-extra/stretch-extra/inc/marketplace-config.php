df504658207b380b9169a0651d0f696cbe305397
Thomas Rose <thomas.rose@ionos.com>
2026-02-06 13:06:28 +0100

    [wip] [skip ci] inital steps

--- packages/wp-mu-plugin/stretch-extra/stretch-extra/inc/marketplace-config.php
+++ packages/wp-mu-plugin/stretch-extra/stretch-extra/inc/marketplace-config.php
@@ -7,7 +7,20 @@ return [
   'ionos_plugins' => [
       'ionos-essentials' =>
       [
-        'url' => 'https://github.com/IONOS-WordPress/ionos-wordpress/releases/download/%40ionos-wordpress%2Flatest/ionos-essentials-info.json'
+        'url' => 'https://github.com/IONOS-WordPress/ionos-wordpress/releases/download/%40ionos-wordpress%2Flatest/ionos-essentials-info.json',
+        'name' => 'Essentials',
+        'short_description' => 'Hier kommen Infos',
+        'version' => 'latest',
+        'author' => 'IONOS',
+        'slug' => 'ionos-essentials',
+        'icons' => [
+          '1x' => 'https://s3-de-central.profitbricks.com/web-hosting/ionos/live/assets/icon-essentials-48px.svg'
+        ],
+        'last_updated' => '2024-05-15 12:00:00',
+        'rating' => 0,
+        'ratings' => [ '5' => 0, '4' => 0, '3' => 0, '2' => 0, '1' => 0 ],
+        'num_ratings' => 0,
+        'active_installs' => 0,
       ],
   'ionos-sso' =>
       [
---
+++ packages/wp-mu-plugin/stretch-extra/stretch-extra/inc/marketplace.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace ionos\stretch_extra\marketplace;
+
+defined('ABSPATH') || exit();
+
+if ( \get_option( 'ionos_group_brand' !== 'ionos' ) ) {
+    return;
+}
+
+add_filter( 'install_plugins_tabs', function ( $tabs ) {
+    unset( $tabs['featured'] );
+
+    return array_merge(
+			[ 'ionos' => 'IONOS ' . __('recommends', 'ionos-stretch-extra') ],
+			$tabs
+		);
+} );
+
+add_action( 'install_plugins_pre_ionos', function () {
+  global $wp_list_table;
+
+  $config = require_once __DIR__ . '/marketplace-config.php';
+
+  // 1. Define the plugin slugs you want
+  $slugs = $config['wordpress_org_plugins'];
+
+  // 2. Build an array of request definitions
+  $field_query_string = '';
+		foreach ( ['short_description','icons'] as $name => $value ) {
+			$field_query_string .= "&fields[{$name}]={$value}";
+		}
+
+  $requests = [];
+  foreach ($slugs as $slug) {
+      $requests[] = [
+          'url'  => "https://api.wordpress.org/plugins/info/1.2/?action=plugin_information&slug=$slug{$field_query_string}",
+          'type' => \WpOrg\Requests\Requests::GET,
+          'data' => [
+            'locale' => get_user_locale(),
+          ],
+      ];
+  }
+
+  // 3. Execute all requests simultaneously
+  $responses = \WpOrg\Requests\Requests::request_multiple($requests);
+
+  // 4. Process the data
+  $plugins = [];
+  foreach ($responses as $slug => $response) {
+      if ($response instanceof \WpOrg\Requests\Response && $response->success) {
+         $wp_list_table->items[] = json_decode($response->body, true);
+      }
+  }
+
+  // 5. Sort items by slug
+  usort($wp_list_table->items, fn($a, $b) => array_search($a['slug'], $slugs) <=> array_search($b['slug'], $slugs));
+
+  // 6. Prepend IONOS Plugins
+
+  array_unshift(
+      $wp_list_table->items,
+      $config['ionos_plugins']['ionos-essentials'],
+  );
+
+} );
+
+add_action( 'install_plugins_ionos', function () {
+    global $wp_list_table;
+
+		$wp_list_table->set_pagination_args(
+			[
+				'total_items' => count( $wp_list_table->items ),
+				'total_pages' => ceil(count( $wp_list_table->items ) / 10),
+				'per_page'    => 10,
+			]
+		);
+
+		display_plugins_table();
+} );
+
+add_action( 'admin_head-plugin-install.php', function () {
+  echo '
+    <style>
+        div[class*="plugin-card-ionos-"] {
+          .column-downloaded, .column-rating {
+            display: none;
+          }
+        }
+    </style>';
+} );
+
+add_filter( 'plugins_api', function ( $result, $action, $args ) {
+   	if ( $action !== 'plugin_information' ) {
+			return $result;
+		}
+
+    $config = require_once __DIR__ . '/marketplace-config.php';
+    if ( ! in_array( $args->slug, array_keys( $config['ionos_plugins'] ), true ) ) {
+      return $result;
+    }
+
+    $response = wp_remote_get( 'https://github.com/IONOS-WordPress/ionos-wordpress/releases/download/%40ionos-wordpress%2Flatest/ionos-essentials-info.json' );
+    $pi       = json_decode( wp_remote_retrieve_body( $response ) );
+    if ( ! is_object( $pi ) ) {
+      return $result;
+    }
+
+    $pi->name          = 'Essentials';
+    $pi->slug          = $args->slug;
+    $pi->download_link = $pi->package;
+    $pi->version       = $pi->version;
+    $pi->requires      = '6.0';
+    $pi->sections      = [
+      _x( 'Description', 'Plugin installer section title' ) => __( $config['ionos_plugins']['ionos-essentials']['short_description'], 'ionos-stretch-extra' ),
+      _x( 'Changelog', 'Plugin installer section title' )   => $pi->sections->changelog,
+    ];
+
+    return $pi;
+}, 20, 3 );
