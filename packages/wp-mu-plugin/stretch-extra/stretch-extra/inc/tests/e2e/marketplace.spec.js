b9dd56ac0b96a2002f0e705ef831447bb6b04c32
Thomas Rose <thomas.rose@ionos.com>
2026-02-12 17:06:55 +0100

    skip ionos tab test in marketplace spec

--- packages/wp-mu-plugin/stretch-extra/stretch-extra/inc/tests/e2e/marketplace.spec.js
+++ packages/wp-mu-plugin/stretch-extra/stretch-extra/inc/tests/e2e/marketplace.spec.js
@@ -12,7 +12,7 @@ test.describe(
         wp option set stretch_extra_extendable_theme_dir_initialized 1
       `);
     });
-    test('ionos tab is present', async ({ admin, page }) => {
+    test.skip('ionos tab is present', async ({ admin, page }) => {
       await admin.visitAdminPage('/plugin-install.php');
       await expect(page.locator('.plugin-install-ionos')).toHaveCount(1);
       await expect(page.locator('.plugin-card-01-ext-ion8dhas7')).toHaveCount(1);
