f1c733d8b19a53036cadcad5943141a3bc159748
Lars Gersmann <lars.gersmann@ionos.com>
2026-02-13 13:02:55 +0100

    fix(marketplace) : fixed e2e test

--- packages/wp-mu-plugin/stretch-extra/stretch-extra/inc/tests/e2e/marketplace.spec.js
+++ packages/wp-mu-plugin/stretch-extra/stretch-extra/inc/tests/e2e/marketplace.spec.js
@@ -9,13 +9,21 @@ test.describe(
   () => {
     test.beforeAll(async () => {
       execTestCLI(`
-        wp option set stretch_extra_extendable_theme_dir_initialized 1
+        # reset stretch-extra theme option
+        wp option delete stretch_extra_extendable_theme_dir_initialized
+        # prevent auto initialization of stretch-extra provisioned plugins
+        wp --quiet option update IONOS_CUSTOM_ACTIVE_PLUGINS_OPTION '[]' --format=json
+        # reset deleted custom plugins
+        wp --quiet option update IONOS_CUSTOM_DELETED_PLUGINS_OPTION '[]' --format=json
+        # marketplace is only be active on group brand ionons
+        wp --quiet option set ionos_group_brand 'ionos'
       `);
     });
-    test.skip('ionos tab is present', async ({ admin, page }) => {
+
+    test('ionos tab is present', async ({ admin, page }) => {
       await admin.visitAdminPage('/plugin-install.php');
       await expect(page.locator('.plugin-install-ionos')).toHaveCount(1);
-      await expect(page.locator('.plugin-card-01-ext-ion8dhas7')).toHaveCount(1);
+      await expect(page.locator('.plugin-card-01-ext-ion8dhas7-stretch')).toHaveCount(1);
     });
   }
 );
