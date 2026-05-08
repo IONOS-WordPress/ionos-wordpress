0d23a409f195fa730ac4b2e242bd0f22204950b1
Thomas Rose <thomas.rose@ionos.com>
2026-05-08 09:10:37 +0200

    zoneid as var

--- packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/blocks/adserver/index.php
+++ packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/blocks/adserver/index.php
@@ -9,7 +9,9 @@ use const ionos\essentials\PLUGIN_FILE;
 function render(): void
 {
   $token = \get_transient('ionos_adserver_token') ?: 'adserver_default_token';
-  $url   = \plugins_url('/ionos-essentials/inc/dashboard/blocks/adserver/view.html?token=' . $token, PLUGIN_FILE);
+  $zoneid =  \wp_get_environment_type() !== 'local' ? 'wp_admin_overview_card_left' : 'developers_docs_example';
+
+  $url   = \plugins_url('/ionos-essentials/inc/dashboard/blocks/adserver/view.html?token=' . $token . '&zoneid=' . $zoneid, PLUGIN_FILE);
   echo '<iframe src="' . esc_url(
     $url
   ) . '" id="adzone" style="display: none; height: 0px; width: 100%;margin-bottom: 32px;border-radius:var(--default-border-radius, 16px);" ></iframe>';
