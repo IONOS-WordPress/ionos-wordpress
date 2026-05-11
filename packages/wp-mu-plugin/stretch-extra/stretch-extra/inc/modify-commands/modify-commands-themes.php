1ecb6a37d2fca86c81c8df0306ba1555ff9f2f58
Denise Stamatov <denise.stamatov@ionos.com>
2026-05-11 16:29:36 +0200

    lint

--- packages/wp-mu-plugin/stretch-extra/stretch-extra/inc/modify-commands/modify-commands-themes.php
+++ packages/wp-mu-plugin/stretch-extra/stretch-extra/inc/modify-commands/modify-commands-themes.php
@@ -3,12 +3,12 @@
 namespace ionos\stretch_extra\secondary_plugin_dir;
 
 // Prevent execution if not in WP-CLI
-if (!defined('WP_CLI') || !WP_CLI) {
-    return;
+if (! defined('WP_CLI') || ! WP_CLI) {
+  return;
 }
 
-if (!defined('IONOS_CUSTOM_DIR')) {
-    return;
+if (! defined('IONOS_CUSTOM_DIR')) {
+  return;
 }
 
 $custom_theme_path = IONOS_CUSTOM_DIR . '/themes';
@@ -17,19 +17,19 @@ $custom_theme_path = IONOS_CUSTOM_DIR . '/themes';
  * Redirect theme root. Ignore /opt/WordPress/
  * Look only at mounted directory.
  */
-\add_filter('theme_root', function($theme_root) use ($custom_theme_path) {
-    return $custom_theme_path;
+\add_filter('theme_root', function ($theme_root) use ($custom_theme_path) {
+  return $custom_theme_path;
 }, 999);
 
 /**
  * Override theme roots. Ignore /opt/WordPress/.
  * Only the mounted path is registered.
  */
-\add_filter('theme_roots', function($roots) use ($custom_theme_path) {
-    // We return an array where only our custom path exists.
-    return array(
-        'themes' => $custom_theme_path
-    );
+\add_filter('theme_roots', function ($roots) use ($custom_theme_path) {
+  // We return an array where only our custom path exists.
+  return [
+    'themes' => $custom_theme_path,
+  ];
 }, 999);
 
 /**
@@ -40,10 +40,10 @@ $custom_theme_path = IONOS_CUSTOM_DIR . '/themes';
 /**
  * Ensures that if WP-CLI tries to verify the theme root, it sees our path.
  */
-\add_filter('pre_option_stylesheet_root', function() use ($custom_theme_path) {
-    return $custom_theme_path;
+\add_filter('pre_option_stylesheet_root', function () use ($custom_theme_path) {
+  return $custom_theme_path;
 });
 
-\add_filter('pre_option_template_root', function() use ($custom_theme_path) {
-    return $custom_theme_path;
+\add_filter('pre_option_template_root', function () use ($custom_theme_path) {
+  return $custom_theme_path;
 });
