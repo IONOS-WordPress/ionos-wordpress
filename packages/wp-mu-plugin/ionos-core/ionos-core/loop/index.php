090a134882f9701e3cb9b0c85ff1816425225649
Thomas Rose <thomas.rose@ionos.com>
2026-06-30 12:15:11 +0200

    lint

--- packages/wp-mu-plugin/ionos-core/ionos-core/loop/index.php
+++ packages/wp-mu-plugin/ionos-core/ionos-core/loop/index.php
@@ -13,10 +13,12 @@ function loop_data_response(\WP_REST_Request $request): \WP_REST_Response
 }
 
 add_action('admin_notices', function () {
-  $my_version = \get_file_data(__DIR__ . '/../../ionos-core.php', ['version' => 'Version'])['version'] ?? null;
-  echo '<div class="notice notice-info is-dismissible"><p>' . esc_html__(
-    'Welcome to IONOS Core. This is version ' . $my_version . '.',
-    'ionos-core'
+  $my_version = \get_file_data(__DIR__ . '/../../ionos-core.php', [
+    'version' => 'Version',
+  ])['version'] ?? null;
+  echo '<div class="notice notice-info is-dismissible"><p>' . \sprintf(
+    \esc_html__('Welcome to IONOS Core. This is version %s.', 'ionos-core'),
+    \esc_html($my_version)
   ) . '</p></div>';
 });
 
