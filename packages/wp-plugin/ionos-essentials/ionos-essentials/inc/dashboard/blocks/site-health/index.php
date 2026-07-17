f21d33b0066470891e152004e889c80651cce80d
Thomas Rose <thomas.rose@ionos.com>
2026-07-17 08:56:53 +0200

    fix: improve site URL handling in site health block

--- packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/blocks/site-health/index.php
+++ packages/wp-plugin/ionos-essentials/ionos-essentials/inc/dashboard/blocks/site-health/index.php
@@ -61,7 +61,16 @@ function render_callback(): void
 
   printf('<i class="%s" style="%s" title="%s: %s"></i>', $icon, $style, $title, $status);
   ?>
-              <h2 class="headline headline--sub"><?php echo \esc_url(parse_url(\get_option('siteurl', ''), PHP_URL_HOST)); ?></h2>
+              <h2 class="headline headline--sub">
+                <?php
+                  $parts = parse_url(\get_option('siteurl', ''));
+  $host                  = $parts['host']   ?? '';
+  $protocol              = $parts['scheme'] ?? '';
+  $unicode               = idn_to_utf8($host, IDNA_DEFAULT, INTL_IDNA_VARIANT_UTS46);
+
+  echo \esc_html(($protocol ? $protocol . '://' : '') . ($unicode ? $unicode : $host));
+  ?>
+              </h2>
             </div>
             <div class="ionos-site-health-overview__info-items">
               <a href="<?php echo \esc_attr(\admin_url(
