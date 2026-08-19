34d4b243e2a6daa53489f0a9a4706f38cdd8f3af
Denise Stamatov <denise.stamatov@ionos.com>
2026-08-19 10:45:54 +0200

    add assets and fix code for core/assets

--- packages/wp-mu-plugin/ionos-core/ionos-core/login/index.php
+++ packages/wp-mu-plugin/ionos-core/ionos-core/login/index.php
@@ -21,13 +21,7 @@ if (defined('IONOS_LOGIN_LOADED')) {
   \add_action(
     'login_enqueue_scripts',
     function () {
-      $assets_file = PLUGIN_DIR . '/ionos-core/build/login/index.asset.php';
-
-      if (! file_exists($assets_file)) {
-        return;
-      }
-
-      $assets  = require_once $assets_file;
+      $assets  = require_once PLUGIN_DIR . '/ionos-core/build/login/index.asset.php';
       $src_url = \plugins_url('ionos-core/build/login/', PLUGIN_FILE);
 
       \wp_enqueue_style('ionos-login-redesign', $src_url . 'index.css', [], $assets['version']);
@@ -56,11 +50,6 @@ if (defined('IONOS_LOGIN_LOADED')) {
         return;
       }
 
-      $essentials_file = WP_PLUGIN_DIR . '/ionos-essentials/ionos-essentials.php';
-      if (! file_exists($essentials_file)) {
-        return;
-      }
-
       printf(
         <<<EOF
         <section class="header">
@@ -73,7 +62,7 @@ if (defined('IONOS_LOGIN_LOADED')) {
 EOF
         ,
         \esc_attr(
-          \plugins_url('ionos-essentials/ionos-essentials/inc/dashboard/data/tenant-logos/' . Tenant::get_slug() . '.svg', $essentials_file)
+          \plugins_url('ionos-core/login/assets/tenant-logos/' . Tenant::get_slug() . '.svg', PLUGIN_FILE)
         ),
         \esc_attr(Tenant::get_label())
       );
